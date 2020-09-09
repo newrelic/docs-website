0a7f21621656ced2b656f65b9b046d0c32c5747b
Zack Stickles <zstickles@newrelic.com>
2020-09-09 16:52:27 -0700

    chore: created frontmatter for mdx files

--- scripts/fetch-docs.js
+++ scripts/fetch-docs.js
@@ -5,6 +5,22 @@ const path = require('path');
 const BASE_URL = 'https://docs.newrelic.com/api/ui/content';
 const BASE_DIR = path.join(__dirname, '..', 'src/content');
 
+const GATSBY_CONTENT_TYPES = {
+  page: 'page',
+  api_doc: 'apiDoc',
+  release_notes: 'releaseNote',
+  release_notes_platform: 'releaseNotePlatform',
+  troubleshooting_doc: 'troubleshootingDoc',
+};
+
+const GATSBY_TEMPLATE = {
+  page: 'basicDocPageTemplate',
+  api_doc: 'basicDocPageTemplate',
+  release_notes: 'releaseNotePageTemplate',
+  release_notes_platform: 'releaseNotePlatformPageTemplate',
+  troubleshooting_doc: 'basicDocPageTemplate',
+};
+
 // IDs grouped by content type (related GH issue in comments)
 const input = [
   { type: 'page', ids: ['38576', '40431'] }, // #44
@@ -36,6 +52,14 @@ const getCategories = (url) => {
     .slice(0, -1);
 };
 
+const getFrontmatter = (type, doc) => `---
+title: ${doc.title}
+contentType: ${GATSBY_CONTENT_TYPES[type]}
+templateKey: ${GATSBY_TEMPLATE[type]}
+---
+
+`;
+
 const fetchPages = async () => {
   // Step 1: get the docs
   const requests = input.flatMap(({ type, ids }) =>
@@ -54,11 +78,21 @@ const fetchPages = async () => {
     // Step 3: create a file for each doc
     const slug = doc.docUrl.split('/').slice(-1);
     const fileName = `${dir}/${slug}.mdx`;
-    fs.writeFile(fileName, doc.body, (err) => {
+
+    // Step 4: create frontmatter based on content type
+    // NOTE: this should be returned in the JSON payload
+    const type = input.find(({ ids }) => ids.includes(doc.docId)).type;
+    const frontmatter = getFrontmatter(type, doc);
+
+    // Step 5: add content to file
+    const content = frontmatter + doc.body;
+    fs.writeFile(fileName, content, (err) => {
       if (err) {
         console.log(`Error, could not create ${fileName}`, e);
       }
     });
+
+    // Step 6: party!
   });
 };
 
