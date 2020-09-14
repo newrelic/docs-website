f4a108704bd04c734e8c3f23112a1bc35b46abee
Cayla Hamann <chamann@newrelic.com>
2020-09-14 14:33:59 -0400

    campfiring on that promise

--- scripts/create-index-pages.js
+++ scripts/create-index-pages.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const logger = require('./utils/logger');
 const { BASE_DIR } = require('./constants');
+const { resolve } = require('path');
 
 const getTitle = (item) =>
   item
@@ -23,11 +24,12 @@ template: basicDoc
 `;
 
 const getPageContent = (dir, files) =>
+  files &&
   files
     .map((file) => {
-      const path = `${dir}/${file}`;
+      const path = `${dir}/${file.name}`;
       // TODO: get the label from frontmatter
-      const label = getTitle(file);
+      const label = getTitle(file.name);
       return `* [${path}](${label})`;
     })
     .join('\n');
@@ -37,14 +39,32 @@ const createIndexPage = (dir) => {
   const title = getTitle(dir);
   const fullDir = path.join(BASE_DIR, dir);
 
-  fs.readdir(fullDir, (err, files) => {
-    if (err) logger.error(`Unabled to read files for ${dir}.`);
+  /* 
+  Desired output: list of links, nested structure 
+  1. Check if there are any directories in this directory
+  2. If there are no directories, create and return a list of links
+  3. If there are directories, create all the links for this folder and then call the function again for the subdirectories
+  */
 
-    const fileName = `${fullDir}/index.mdx`;
-    const content = getFrontmatter(title) + getPageContent(dir, files);
-    fs.writeFile(fileName, content, (err) => {
-      if (err) logger.error(`Could not create ${fileName}.`);
+  const linkContent = new Promise((resolve, reject) => {
+    fs.readdir(fullDir, { withFileTypes: true }, (err, files) => {
+      if (err) logger.error(`Unabled to read files for ${dir}.`);
+
+      const subDirs = files && files.filter((file) => file.isDirectory());
+      const subLinkContent =
+        subDirs &&
+        subDirs.reduce((content, subDir) => {
+          return content + createIndexPage(subDir.name);
+        }, '');
+      const currentLinkContent = getPageContent(dir, files);
+
+      return resolve(currentLinkContent + subLinkContent);
     });
+  });
+  const fileName = `${fullDir}/index.mdx`;
+  const content = getFrontmatter(title) + linkContent;
+  fs.writeFile(fileName, content, (err) => {
+    if (err) logger.error(`Could not create ${fileName}.`);
   });
 };
 
