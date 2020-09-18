af489fb919088d2ed419d95a3a887fd64b2f5a86
Zack Stickles <zstickles@newrelic.com>
2020-09-18 10:20:50 -0700

    chore: script to convert docs

---
+++ scripts/convert-docs.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+const TurndownService = require('turndown');
+
+const getCategories = require('./utils/get-categories');
+const {
+  BASE_DIR,
+  GATSBY_CONTENT_TYPES,
+  GATSBY_TEMPLATE,
+} = require('./constants');
+
+const getFrontmatter = (type, doc) => `---
+title: ${doc.title.replace(':', '-')}
+contentType: ${GATSBY_CONTENT_TYPES[type]}
+template: ${GATSBY_TEMPLATE[type]}
+---
+
+`;
+
+const convertDocs = (docs) => {
+  docs.flat().forEach((doc) => {
+    const dir = path.join(BASE_DIR, ...getCategories(doc.docUrl));
+    const slug = doc.docUrl.split('/').slice(-1);
+    const fileName = `${dir}/${slug}.mdx`;
+
+    // Create frontmatter based on content type
+    const frontmatter = getFrontmatter(doc.type, doc);
+
+    // Convert content to markdown
+    const turndownService = new TurndownService({
+      headingStyle: 'atx',
+    });
+
+    turndownService.addRule('codeBlocks', {
+      filter: ['pre'],
+      replacement: function (content) {
+        return `~~~\n${content}\n~~~\n`;
+      },
+    });
+
+    turndownService.keep(['table']);
+
+    const bodyContent = doc.body ? turndownService.turndown(doc.body) : '';
+    const content = frontmatter + bodyContent;
+
+    // Write the file
+    fs.writeFile(fileName, content, (err) => {
+      if (err) logger.error(`Could not create ${fileName}.`);
+    });
+  });
+};
+
+module.exports = convertDocs;
