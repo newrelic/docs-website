f560756dfb52beb73a04cc4b8a0c0b7e0c527121
Zack Stickles <zstickles@newrelic.com>
2020-09-21 16:39:46 -0700

    chore: make `toJSON` more abstract

--- scripts/converters/to-json.js
+++ scripts/converters/to-json.js
@@ -1,20 +1,23 @@
 const path = require('path');
 const getCategories = require('../utils/get-categories');
-const { BASE_DIR } = require('../constants');
+const { TYPES, BASE_DIR } = require('../constants');
+
+const JSON_BY_TYPE = {
+  [TYPES.ATTRIBUTE_DEFINITION]: (doc) => ({
+    id: doc.docId,
+    title: doc.title,
+    description: doc.shortDescription,
+    eventTypes: doc.eventTypes,
+    relatedInfo: doc.relatedInfo,
+  }),
+};
 
 const toJSON = (doc) => {
   const dir = path.join(BASE_DIR, ...getCategories(doc.docUrl));
   const slug = doc.docUrl.split('/').slice(-1);
   const fileName = `${dir}/${slug}.json`;
 
-  const json = {
-    id: doc.docId,
-    title: doc.title,
-    description: doc.shortDescription,
-    eventTypes: doc.eventTypes,
-    relatedInfo: doc.relatedInfo,
-  };
-
+  const json = JSON_BY_TYPE[doc.type](doc);
   const content = JSON.stringify(json, null, 2);
 
   return { content, fileName };
