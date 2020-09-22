43d07ba4caf827a1ac54c7458cc27d8218610dbc
Cayla Hamann <chamann@newrelic.com>
2020-09-22 17:38:37 -0400

    feat: added templates for different content types

---
+++ src/templates/whatsNew.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import { MDXProvider } from '@mdx-js/react';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+
+const whatsNewTemplate = ({ data }) => {
+  const { mdx } = data;
+  const { frontmatter, body } = mdx;
+  return (
+    <>
+      <h1>{frontmatter.title}</h1>
+      <MDXProvider>
+        <MDXRenderer>{body}</MDXRenderer>
+      </MDXProvider>
+    </>
+  );
+};
+
+whatsNewTemplate.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export const pageQuery = graphql`
+  query($fileRelativePath: String!) {
+    mdx(fields: { fileRelativePath: { eq: $fileRelativePath } }) {
+      body
+      frontmatter {
+        title
+        topics
+        summary
+        learnMoreLink
+        getStartedLink
+      }
+    }
+  }
+`;
+
+export default whatsNewPlatformTemplate;
