0fc1befda31ce8615b379523bafd843c7f331113
Jerel Miller <jmiller@newrelic.com>
2020-09-22 14:25:36 -0700

    chore: add example for codemodding headings

--- codemods/index.js
+++ codemods/index.js
@@ -1,7 +1,8 @@
 const coolButton = require('./cool-button');
 const coolButton2 = require('./cool-button-2');
+const uppercaseHeadings = require('./uppercase-headings');
 
 module.exports = {
-  mdx: [],
+  mdx: [uppercaseHeadings],
   jsx: [coolButton, coolButton2],
 };
