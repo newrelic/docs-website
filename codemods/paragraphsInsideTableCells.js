f40db3ddc3c783df54dad4a58729c5a16d40d200
Jerel Miller <jmiller@newrelic.com>
2020-09-29 02:18:27 -0700

    chore: handle paragraphs that proceed lists

--- codemods/paragraphsInsideTableCells.js
+++ codemods/paragraphsInsideTableCells.js
@@ -1,9 +1,13 @@
 const visit = require('unist-util-visit');
+const visitParents = require('unist-util-visit-parents');
 const {
   flatten,
+  hasAttribute,
   isMdxBlockElement,
   isMdxElement,
   hasOnlyChild,
+  removeAttribute,
+  removeChild,
 } = require('./utils/mdxast');
 
 const paragraphsInsideTableCells = () => (tree) => {
@@ -15,8 +19,32 @@ const paragraphsInsideTableCells = () => (tree) => {
       flatten(td);
     }
 
+    visitParents(
+      td,
+      (node) =>
+        isMdxElement('p', node) && hasAttribute('data-fixnewline', node),
+      (p, ancestors) => {
+        const parent = ancestors[ancestors.length - 1];
+        const list = ancestors.find((node) => node.type === 'list');
+        const listParent = ancestors[ancestors.indexOf(list) - 1];
+        const listIdx = listParent.children.indexOf(list);
+
+        listParent.children = [
+          ...listParent.children.slice(0, listIdx + 1),
+          p,
+          ...listParent.children.slice(listIdx + 1),
+        ];
+
+        removeAttribute('data-fixnewline', p);
+        removeChild(p, parent);
+      }
+    );
+
     visit(td, isMdxElement('p'), (p) => {
       p.type = 'paragraph';
+
+      delete p.name;
+      delete p.attributes;
     });
   });
 };
