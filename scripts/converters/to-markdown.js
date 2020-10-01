2dad345c929b0f008be68c260b1b3db5637d7c7d
Jerel Miller <jmiller@newrelic.com>
2020-09-30 23:56:21 -0700

    refactor: refactor to-markdown to work with vfile

--- scripts/converters/to-markdown.js
+++ scripts/converters/to-markdown.js
@@ -1,23 +1,8 @@
-const path = require('path');
 const htmlToMarkdown = require('../utils/htmlToMarkdown');
-
-const getCategories = require('../utils/get-categories');
 const getFrontmatter = require('../frontmatter/get-frontmatter');
-const { TYPES, BASE_DIR } = require('../constants');
 
-const toMarkdown = (doc) => {
-  const dir = path.join(BASE_DIR, ...getCategories(doc.docUrl));
-  const slug = doc.docUrl.split('/').slice(-1);
-  const fileName =
-    doc.type === TYPES.LANDING_PAGE ? `${dir}/index.mdx` : `${dir}/${slug}.mdx`;
-
-  // Create frontmatter based on content type
-  const frontmatter = getFrontmatter(doc.type, doc);
-
-  const bodyContent = doc.body ? htmlToMarkdown(doc.body) : '';
-  const content = frontmatter + bodyContent;
-
-  return { content, fileName };
+const toMarkdown = (file) => {
+  file.contents = getFrontmatter(file.meta.doc) + htmlToMarkdown(file.contents);
 };
 
 module.exports = toMarkdown;
