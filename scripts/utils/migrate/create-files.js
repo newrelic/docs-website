eb2d2c57ef09c87d1c7f85964c95efdde9ae5302
Jerel Miller <jmiller@newrelic.com>
2020-09-30 23:18:38 -0700

    refactor: only map vfiles in createFiles but do not create directories

--- scripts/utils/migrate/create-files.js
+++ scripts/utils/migrate/create-files.js
@@ -1,7 +1,5 @@
-const fs = require('fs');
 const path = require('path');
 const vfile = require('vfile');
-const getCategories = require('../get-categories');
 const { BASE_DIR, TYPES } = require('../../constants');
 
 const DEFAULT_EXTENSION = '.mdx';
@@ -12,16 +10,16 @@ const EXTENSIONS = {
 
 const createFiles = (docs) => {
   return docs.map((doc) => {
-    const dirname = path.join(BASE_DIR, ...getCategories(doc.docUrl));
+    const url = new URL(doc.docUrl);
+    const pathSegments = url.pathname.replace(/^\//, '').split('/');
 
-    if (!fs.existsSync(dirname)) {
-      fs.mkdirSync(dirname, { recursive: true });
-    }
-
-    const slug = doc.docUrl.split('/').slice(-1);
+    const dirname = path.join(BASE_DIR, ...pathSegments.slice(0, -1));
 
     return vfile({
-      stem: doc.type === TYPES.LANDING_PAGE ? 'index' : slug,
+      stem:
+        doc.type === TYPES.LANDING_PAGE
+          ? 'index'
+          : pathSegments[pathSegments.length - 1],
       extname: EXTENSIONS[doc.type] || DEFAULT_EXTENSION,
       dirname,
       contents: doc.body || '',
