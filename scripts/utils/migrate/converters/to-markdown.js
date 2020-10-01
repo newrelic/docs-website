3a10fab5ccfe8c9f9be16565e0533112fe54deb5
Jerel Miller <jmiller@newrelic.com>
2020-10-01 00:05:46 -0700

    chore: move get-frontmatter to scripts/utils/migrate

--- scripts/utils/migrate/converters/to-markdown.js
+++ scripts/utils/migrate/converters/to-markdown.js
@@ -1,5 +1,5 @@
 const htmlToMarkdown = require('../html-to-markdown');
-const getFrontmatter = require('../../../frontmatter/get-frontmatter');
+const getFrontmatter = require('../get-frontmatter');
 
 const toMarkdown = (file) => {
   file.contents = getFrontmatter(file.meta.doc) + htmlToMarkdown(file.contents);
