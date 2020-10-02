92ea69ac18e9f7a419a76882fe84e4cd716a77a7
Jerel Miller <jmiller@newrelic.com>
2020-10-02 01:11:30 -0700

    feat: add a clamshell codemod to convert clamshells to our new collapser component

--- codemods/index.js
+++ codemods/index.js
@@ -1,11 +1,13 @@
 const indentedCodeBlock = require('./indentedCodeBlock');
 const paragraphsInsideTableCells = require('./paragraphsInsideTableCells');
 const callouts = require('./callouts');
+const clamshells = require('./clamshells');
 const videos = require('./videos');
 
 module.exports = [
   paragraphsInsideTableCells,
   indentedCodeBlock,
   callouts,
+  clamshells,
   videos,
 ];
