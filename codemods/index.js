e9941b16c5e54fce0d9deffc335133d248784b72
Cayla Hamann <chamann@newrelic.com>
2020-10-05 17:41:17 -0400

    feat: transformed table component and added codemod

--- codemods/index.js
+++ codemods/index.js
@@ -3,6 +3,7 @@ const paragraphsInsideTableCells = require('./paragraphsInsideTableCells');
 const callouts = require('./callouts');
 const clamshells = require('./clamshells');
 const videos = require('./videos');
+const tables = require('./tables');
 
 module.exports = [
   paragraphsInsideTableCells,
@@ -10,4 +11,5 @@ module.exports = [
   callouts,
   clamshells,
   videos,
+  tables,
 ];
