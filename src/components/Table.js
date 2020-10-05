7a1b8db7e5430c4b2ffd83c337b35031bbb53682
Cayla Hamann <chamann@newrelic.com>
2020-10-05 17:13:53 -0400

    feat: table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,19 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 
-const dummyTable = () => <></>;
-
 const Table = () => (
   <table
     css={css`
-      thead {
-        border-bottom: 0.375rem solid #52bbbb;
-      }
-      width: 700px;
+      border-collapse: collapse;
+      border-spacing: 0;
+      width: 100%;
       text-align: left;
       td,
       th {
         padding: 0.5rem;
+      }
+      th {
+        border-bottom: 3px solid var(--color-brand-600);
+      }
+      tbody {
+        tr {
+          border-bottom: 1px solid var(--border-color);
+        }
+        tr:nth-child(odd) {
+          background-color: var(--color-neutrals-100);
+          .dark-mode & {
+            background-color: var(--color-dark-100);
+          }
+        }
       }
     `}
   >
@@ -26,6 +37,22 @@ const Table = () => (
       </tr>
     </thead>
     <tbody>
+      <tr>
+        <td>Jill</td>
+        <td>
+          <div>
+            <ul>
+              <li>hello there</li>
+            </ul>
+          </div>
+        </td>
+        <td>50</td>
+      </tr>
+      <tr>
+        <td>Eve</td>
+        <td>Jackson</td>
+        <td>94</td>
+      </tr>
       <tr>
         <td>Jill</td>
         <td>
