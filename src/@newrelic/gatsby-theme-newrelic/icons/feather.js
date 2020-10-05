21b182b9c615f44c6e5cf3f488f456c5ab1cc148
Jerel Miller <jmiller@newrelic.com>
2020-10-05 15:50:48 -0700

    chore: add zap icon

--- src/@newrelic/gatsby-theme-newrelic/icons/feather.js
+++ src/@newrelic/gatsby-theme-newrelic/icons/feather.js
@@ -4,4 +4,5 @@ import defaultIcons from '@newrelic/gatsby-theme-newrelic/src/icons/feather';
 export default {
   ...defaultIcons,
   'chevron-down': <polyline points="6 9 12 15 18 9" />,
+  zap: <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2" />,
 };
