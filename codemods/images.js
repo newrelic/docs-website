63d69d48c91a702d69454398fe9ad260d2922d1e
Clinton <clinton.langosch@gmail.com>
2020-10-06 11:58:09 -0700

    chore: Save image in image subfolder in same directory as file referencing the image

--- codemods/images.js
+++ codemods/images.js
@@ -1,25 +1,35 @@
 const visit = require('unist-util-visit');
 const download = require('image-downloader');
 const path = require('path');
+const fs = require('fs');
 
-const { setAttribute } = require('./utils/mdxast');
+const { setAttribute, isType } = require('./utils/mdxast');
 
 const images = () => (tree, file) => {
   visit(
     tree,
-    (node) => node.type === 'image',
+    (node) => isType('image', node),
     (image) => {
+      const currDirectoryPath = file.history[0].substring(
+        0,
+        file.history[0].lastIndexOf('/')
+      );
+      const imageDirectory = path.join(currDirectoryPath, 'images');
+      if (!fs.existsSync(imageDirectory)) {
+        fs.mkdirSync(imageDirectory);
+      }
+
       const downloadImgUrl = image.url.includes('.newrelic.com/')
         ? image.url
         : `https://docs-dev.newrelic.com${image.url}`;
       const options = {
         url: downloadImgUrl,
-        dest: path.join('src/images'),
+        dest: imageDirectory,
       };
       try {
         const downloadImage = download.image(options);
         console.log(`Image downloaded: ${downloadImgUrl}`);
-        const gatsbyImgUrl = `/images/${downloadImgUrl.split('image/')[1]}`;
+        const gatsbyImgUrl = `./images/${downloadImgUrl.split('image/')[1]}`;
         image.url = gatsbyImgUrl;
       } catch (error) {
         file.fail(
