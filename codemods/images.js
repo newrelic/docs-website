e03ab12f1e174fb3ffc6ef3779d435d99175548e
Jerel Miller <jmiller@newrelic.com>
2020-10-06 12:58:01 -0700

    chore: handle all async image downloads

--- codemods/images.js
+++ codemods/images.js
@@ -2,32 +2,37 @@ const visit = require('unist-util-visit');
 const download = require('image-downloader');
 const path = require('path');
 const fs = require('fs');
+const logger = require('../scripts/utils/logger');
 const { BASE_URL } = require('../scripts/utils/constants');
+const { isType } = require('./utils/mdxast');
 
-const { setAttribute, isType } = require('./utils/mdxast');
+const images = () => async (tree, file) => {
+  const promises = [];
 
-const images = () => (tree, file) => {
   visit(
     tree,
     (node) => isType('image', node),
-    (image) => {
-      console.log(file.dirname);
-      const currDirectoryPath = file.dirname;
-      const imageDirectory = path.join(currDirectoryPath, 'images');
+    async (image) => {
+      const imageDirectory = path.join(file.dirname, 'images');
+
       if (!fs.existsSync(imageDirectory)) {
         fs.mkdirSync(imageDirectory);
       }
-      console.log(BASE_URL);
+
       const downloadImgUrl = image.url.includes('.newrelic.com/')
         ? image.url
         : path.join(BASE_URL, image.url);
-      const options = {
-        url: downloadImgUrl,
-        dest: imageDirectory,
-      };
+
       try {
-        const downloadImage = download.image(options);
-        console.log(`Image downloaded: ${downloadImgUrl}`);
+        promises.push(
+          download.image({
+            url: downloadImgUrl,
+            dest: imageDirectory,
+          })
+        );
+
+        logger.normal(`Image downloaded: ${downloadImgUrl}`);
+
         const gatsbyImgUrl = `./images/${downloadImgUrl.split('image/')[1]}`;
         image.url = gatsbyImgUrl;
       } catch (error) {
@@ -38,6 +43,8 @@ const images = () => (tree, file) => {
       }
     }
   );
+
+  await Promise.all(promises);
 };
 
 module.exports = images;
