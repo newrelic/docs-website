f3ceaef693dfaf8f9f86e744be621b91b6f71144
Jerel Miller <jmiller@newrelic.com>
2020-10-07 12:18:40 -0700

    chore: fix error handling of image downloads causing unhandled rejection issues

--- codemods/images.js
+++ codemods/images.js
@@ -3,45 +3,53 @@ const download = require('image-downloader');
 const path = require('path');
 const fs = require('fs');
 const { BASE_URL } = require('../scripts/utils/constants');
-const { isType } = require('./utils/mdxast');
+const convert = require('unist-util-is/convert');
+
+const isImage = convert('image');
 
 const images = () => async (tree, file) => {
   const promises = [];
+  const imageDirectory = path.join(file.dirname, 'images');
 
-  visit(
-    tree,
-    (node) => isType('image', node),
-    async (image) => {
-      const imageDirectory = path.join(file.dirname, 'images');
+  if (!fs.existsSync(imageDirectory)) {
+    fs.mkdirSync(imageDirectory);
+  }
 
-      if (!fs.existsSync(imageDirectory)) {
-        fs.mkdirSync(imageDirectory);
-      }
+  const downloadImage = async (node) => {
+    const url = node.url.startsWith('http')
+      ? node.url
+      : path.join(BASE_URL, node.url);
 
-      const downloadImgUrl = image.url.includes('.newrelic.com/')
-        ? image.url
-        : path.join(BASE_URL, image.url);
+    try {
+      const { filename } = await download.image({
+        url,
+        dest: imageDirectory,
+      });
 
-      try {
-        const promise = download.image({
-          url: downloadImgUrl,
-          dest: imageDirectory,
-        });
-
-        promises.push(promise);
-
-        const { filename } = await promise;
-
-        // eslint-disable-next-line require-atomic-updates
-        image.url = `./images/${path.basename(filename)}`;
-      } catch (error) {
-        file.fail(
-          new Error(`Error saving file at url: ${downloadImgUrl}`),
-          image.position
-        );
-      }
+      return filename;
+    } catch (e) {
+      file.fail(
+        `Error downloading file: ${url}`,
+        node.position.start,
+        'images'
+      );
     }
-  );
+  };
+
+  visit(tree, isImage, async (image) => {
+    try {
+      const promise = downloadImage(image);
+
+      promises.push(promise);
+
+      const filename = await promise;
+
+      // eslint-disable-next-line require-atomic-updates
+      image.url = `./images/${path.basename(filename)}`;
+    } catch (error) {
+      // do nothing
+    }
+  });
 
   await Promise.all(promises);
 };
