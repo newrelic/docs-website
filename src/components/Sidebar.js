79f37cc142346df8b2951cffa73269916f0cca13
Jerel Miller <jmiller@newrelic.com>
2020-10-19 14:06:00 -0700

    feat: add a sidebar to the layout

---
+++ src/components/Sidebar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
+import { Link } from 'gatsby';
+
+const Sidebar = ({ className }) => (
+  <aside
+    data-swiftype-index={false}
+    className={className}
+    css={css`
+      padding: 0 2rem 2rem;
+      border-right: 1px solid var(--divider-color);
+
+      @media screen and (max-width: 760px) {
+        height: 60px;
+        border-bottom: 1px solid var(--divider-color);
+      }
+    `}
+  >
+    <div
+      css={css`
+        background: var(--primary-background-color);
+        position: sticky;
+        top: 0;
+        z-index: 10;
+        padding: 1rem 0.5rem;
+        margin: 0 -0.5rem 0.5rem;
+      `}
+    >
+      <Link
+        css={css`
+          display: block;
+          margin-bottom: 1rem;
+        `}
+        to="/"
+      >
+        Logo
+      </Link>
+    </div>
+    <div>Navigation goes here</div>
+  </aside>
+);
+Sidebar.propTypes = {
+  className: PropTypes.string,
+};
+
+export default Sidebar;
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { GlobalHeader } from '@newrelic/gatsby-theme-newrelic';
 import { graphql, useStaticQuery } from 'gatsby';
 import { css } from '@emotion/core';
+import Sidebar from '../components/Sidebar';
 
 const MainLayout = ({ children }) => {
   const {
@@ -19,18 +20,69 @@ const MainLayout = ({ children }) => {
   `);
 
   return (
-    <>
+    <div
+      css={css`
+        --global-header-height: 36px;
+        --sidebar-width: 300px;
+
+        min-height: 100vh;
+      `}
+    >
       <GlobalHeader />
-      <main
+      <div
         css={css`
           margin: 0 auto;
           max-width: ${layout.maxWidth};
-          padding: ${layout.contentPadding};
+          display: grid;
+          grid-template-areas:
+            'sidebar content'
+            'sidebar footer';
+          grid-template-columns: var(--sidebar-width) minmax(0, 1fr);
+          grid-template-rows: 1fr auto;
+          grid-gap: ${layout.contentPadding};
+          width: 100%;
+          max-width: ${layout.maxWidth};
+          margin: 0 auto;
+
+          @media screen and (max-width: 760px) {
+            grid-template-columns: minmax(0, 1fr);
+          }
         `}
       >
-        {children}
-      </main>
-    </>
+        <Sidebar
+          css={css`
+            position: fixed;
+            top: var(--global-header-height);
+            width: var(--sidebar-width);
+            height: calc(100vh - var(--global-header-height));
+            overflow: auto;
+
+            @media (max-width: 760px) {
+              display: none;
+            }
+          `}
+        />
+        <div
+          css={css`
+            grid-area: sidebar;
+          `}
+        />
+        <main
+          css={css`
+            grid-area: content;
+            padding-top: ${layout.contentPadding};
+            padding-right: ${layout.contentPadding};
+          `}
+        >
+          {children}
+        </main>
+        <footer
+          css={css`
+            grid-area: footer;
+          `}
+        />
+      </div>
+    </div>
   );
 };
 
