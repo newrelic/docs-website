5731e88b9a41976260c18b8ee7a453742b3fa6cd
Jerel Miller <jmiller@newrelic.com>
2020-10-20 12:04:32 -0700

    chore: handle moving categories and creating intermediate structure

--- scripts/utils/migrate/migrate-nav-structure.js
+++ scripts/utils/migrate/migrate-nav-structure.js
@@ -3,6 +3,7 @@ const vfile = require('vfile');
 const path = require('path');
 const slugify = require('../slugify');
 const { NAV_DIR } = require('../constants');
+const logger = require('../logger');
 
 const TYPES = {
   MOVE: 'MOVE',
@@ -24,6 +25,8 @@ const migrateNavStructure = (files) => {
     switch (instruction.type) {
       case TYPES.MOVE:
         return move(files, instruction);
+      case TYPES.REMOVE:
+        return remove(files, instruction);
       default:
         throw new Error(`Unknown instruction: ${instruction.type}`);
     }
@@ -36,32 +39,102 @@ const move = (files, { from, to }) => {
 
   const sourceFile = files.find((file) => file.basename === sourceFileName);
 
+  if (!sourceFile) {
+    logger.warn(`File not found: ${path.join(NAV_DIR, sourceFileName)}`);
+
+    return files;
+  }
+
   const nav = yaml.safeLoad(sourceFile.contents);
   const child = findCategory(nav, subtopics);
 
   const destinationFileName = `${slugify(to[0] || child.title)}.yml`;
+  let destinationFile = files.find(
+    (file) => file.basename === destinationFileName
+  );
 
   if (to.length === 0) {
-    const destinationFile = vfile({
+    destinationFile = vfile({
       path: path.join(NAV_DIR, destinationFileName),
       contents: yaml.safeDump(child, { lineWidth: 99999 }),
     });
 
     files = [...files, destinationFile];
+
+    return remove(files, { path: from });
   }
 
+  if (!destinationFile) {
+    destinationFile = vfile({
+      path: path.join(NAV_DIR, destinationFileName),
+      contents: yaml.safeDump(
+        { title: to[0], children: [] },
+        { lineWidth: 9999 }
+      ),
+    });
+
+    files = [...files, destinationFile];
+  }
+
+  const updatedNav = addChild(
+    child,
+    yaml.safeLoad(destinationFile.contents),
+    to.slice(1)
+  );
+
+  destinationFile.contents = yaml.safeDump(updatedNav, { lineWidth: 9999 });
+
   return remove(files, { path: from });
+};
+
+const addChild = (node, parent, topics) => {
+  const [title, ...subtopics] = topics;
+  const { children = [] } = parent;
+  const idx = children.findIndex((node) => node.title === title);
+
+  if (topics.length === 0) {
+    return {
+      ...parent,
+      children: [...children, node],
+    };
+  }
+
+  if (idx === -1) {
+    return {
+      ...parent,
+      children: [
+        ...children,
+        addChild(node, { title, children: [] }, subtopics),
+      ],
+    };
+  }
+
+  return {
+    ...parent,
+    children: [
+      ...children.slice(0, idx),
+      addChild(node, children[idx], subtopics),
+      ...children.slice(idx + 1),
+    ],
+  };
 };
 
 const remove = (files, { path }) => {
   const [title, ...subtopics] = path;
   const sourceFileName = `${slugify(title)}.yml`;
   const sourceFile = files.find((file) => file.basename === sourceFileName);
+
+  if (!sourceFile) {
+    logger.warn(`File not found: ${path.join(NAV_DIR, sourceFileName)}`);
+
+    return files;
+  }
+
   const nav = yaml.safeLoad(sourceFile.contents);
   const updatedNav = filterCategory(nav, subtopics);
 
   if (updatedNav.children.length === 0) {
-    return files.filter((file) => file === sourceFile);
+    return files.filter((file) => file !== sourceFile);
   }
 
   sourceFile.contents = yaml.safeDump(updatedNav, { lineWidth: 9999 });
@@ -79,9 +152,16 @@ const filterCategory = (nav, topics) => {
     };
   }
 
-  const child = nav.children.find((child) => child.title === title);
+  const idx = nav.children.findIndex((child) => child.title === title);
 
-  return filterCategory(child, subtopics);
+  return {
+    ...nav,
+    children: [
+      ...nav.children.slice(0, idx),
+      filterCategory(nav.children[idx], subtopics),
+      ...nav.children.slice(idx + 1),
+    ],
+  };
 };
 
 const findCategory = (nav, topics) => {
