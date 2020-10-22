c35b01f766cae78063e7b69ce318694d42b6bc00
Jerel Miller <jmiller@newrelic.com>
2020-10-21 22:38:33 -0700

    refactor: extract NavLink to own component

---
+++ src/components/NavLink.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
+import { Link } from 'gatsby';
+import { Icon } from '@newrelic/gatsby-theme-newrelic';
+
+const NavLink = ({
+  as: Element = Link,
+  title,
+  depth,
+  isExpanded,
+  expandable,
+  onClick,
+  ...props
+}) => {
+  return (
+    <Element
+      {...props}
+      onClick={onClick}
+      css={css`
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        cursor: pointer;
+        color: var(--primary-text-color);
+        transition: 0.2s ease-out;
+        padding: 0.5rem 0;
+        padding-left: ${depth * 1}rem;
+        font-size: 0.875rem;
+
+        &:hover {
+          color: var(--primary-text-hover-color);
+        }
+      `}
+    >
+      <span
+        css={css`
+          flex: 1;
+        `}
+      >
+        {title}
+      </span>
+
+      {expandable && (
+        <Icon
+          name="chevron-down"
+          size="1rem"
+          css={css`
+            transform: rotate(${isExpanded ? 0 : -90}deg);
+            transition: 0.2s ease-out;
+          `}
+        />
+      )}
+    </Element>
+  );
+};
+
+NavLink.propTypes = {
+  as: PropTypes.elementType,
+  title: PropTypes.string.isRequired,
+  depth: PropTypes.number.isRequired,
+  isExpanded: PropTypes.bool.isRequired,
+  expandable: PropTypes.bool.isRequired,
+  onClick: PropTypes.func,
+};
+
+export default NavLink;
