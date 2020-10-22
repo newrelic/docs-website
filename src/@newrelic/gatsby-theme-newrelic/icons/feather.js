95afa36a47a86fb652c748e465918a4bffec3a86
Jerel Miller <jmiller@newrelic.com>
2020-10-21 17:21:09 -0700

    chore: add arrow-left-circle icon

--- src/@newrelic/gatsby-theme-newrelic/icons/feather.js
+++ src/@newrelic/gatsby-theme-newrelic/icons/feather.js
@@ -3,6 +3,13 @@ import defaultIcons from '@newrelic/gatsby-theme-newrelic/src/icons/feather';
 
 export default {
   ...defaultIcons,
+  'arrow-left-circle': (
+    <>
+      <circle cx="12" cy="12" r="10" />
+      <polyline points="12 8 8 12 12 16" />
+      <line x1="16" y1="12" x2="8" y2="12" />
+    </>
+  ),
   'alert-triangle': (
     <>
       <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z" />
