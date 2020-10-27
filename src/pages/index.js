f342b0e7ed453db2e93273420c161c684dd68dfc
Jerel Miller <jmiller@newrelic.com>
2020-10-27 00:42:00 -0700

    chore: move data to yaml file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Children } from 'react';
 import PropTypes from 'prop-types';
 import { css, keyframes } from '@emotion/core';
 import { Link, graphql } from 'gatsby';
@@ -9,6 +9,7 @@ import FSOIcon from '../components/FSOIcon';
 import TDPIcon from '../components/TDPIcon';
 import IntegrationIcon from '../components/IntegrationIcon';
 import SurfaceLink from '../components/SurfaceLink';
+import { tdp, fso, ai, security } from '../data/homepage.yml';
 
 const HomePage = ({ data }) => {
   const {
@@ -90,39 +91,28 @@ const HomePage = ({ data }) => {
         </div>
       </Section>
       <Section alternate layout={layout}>
-        <SectionTitle title="Telemetry Data Platform" icon={TDPIcon} />
-        <SectionDescription>
-          Ingest, visualize, and alert on all your telemetry data in one place.
-        </SectionDescription>
+        <SectionTitle title={tdp.title} icon={TDPIcon} />
+        <SectionDescription>{tdp.description}</SectionDescription>
         <DocTileGrid>
-          {tdpGuides.map((guide, idx) => (
+          {tdp.tiles.map((guide, idx) => (
             <DocTile key={idx} guide={guide} />
           ))}
         </DocTileGrid>
       </Section>
       <Section layout={layout}>
-        <SectionTitle title="Full-Stack Observability" icon={FSOIcon} />
-        <SectionDescription>
-          Analyze and troubleshoot problems easily across your entire software
-          stack.
-        </SectionDescription>
+        <SectionTitle title={fso.title} icon={FSOIcon} />
+        <SectionDescription>{fso.description}</SectionDescription>
         <DocTileGrid>
-          {fsoGuides.map((guide, idx) => (
+          {fso.tiles.map((guide, idx) => (
             <DocTile key={idx} guide={guide} />
           ))}
         </DocTileGrid>
       </Section>
       <Section alternate layout={layout}>
-        <SectionTitle
-          title="Alerts and Applied Intelligence (AI)"
-          icon={AIIcon}
-        />
-        <SectionDescription>
-          Automatically detect anomalies, correlate issues, and reduce alert
-          noise.
-        </SectionDescription>
+        <SectionTitle title={ai.title} icon={AIIcon} />
+        <SectionDescription>{ai.description}</SectionDescription>
         <DocTileGrid>
-          {aiGuides.map((guide, idx) => (
+          {ai.tiles.map((guide, idx) => (
             <DocTile key={idx} guide={guide} />
           ))}
         </DocTileGrid>
@@ -333,13 +323,10 @@ const HomePage = ({ data }) => {
         </div>
       </Section>
       <Section layout={layout}>
-        <SectionTitle title="Security, privacy, and legal information" />
-        <SectionDescription>
-          Find out how we ensure security, data privacy, and compliance, and
-          find our terms of service.
-        </SectionDescription>
-        <DocTileGrid maxTiles={4}>
-          {securityGuides.map((guide, idx) => (
+        <SectionTitle title={security.title} />
+        <SectionDescription>{security.description}</SectionDescription>
+        <DocTileGrid>
+          {security.tiles.map((guide, idx) => (
             <DocTile key={idx} guide={guide} />
           ))}
         </DocTileGrid>
@@ -368,175 +355,6 @@ export const pageQuery = graphql`
     ...MainLayout_query
   }
 `;
-
-const tdpGuides = [
-  {
-    title: 'Introduction to Telemetry Data Platform',
-    description: 'How to manage all your monitoring in one place.',
-    to:
-      '/docs/data-ingest-apis/get-data-new-relic/getting-started/get-started-telemetry-data-platform',
-  },
-  {
-    title: 'Data explorer',
-    description:
-      'Query and build charts with NRQL, our PromQL-style syntax, or our visual chart builder.',
-    to:
-      '/docs/query-your-data/explore-query-data/data-explorer/introduction-data-explorer',
-  },
-  {
-    title: 'Dashboards',
-    description:
-      'Combine data from anywhere in our platform into customized dashboards.',
-    to:
-      '/docs/dashboards/new-relic-one-dashboards/get-started/introduction-new-relic-one-dashboards',
-  },
-  {
-    title: 'Log management',
-    description:
-      'Get logs alongside your telemetry data with our fast, scalable log management.',
-    to: '/docs/logs/new-relic-logs/get-started/introduction-new-relic-logs',
-  },
-  {
-    title: 'APIs',
-    description:
-      'Find APIs to send data in, get data out, or manage the New Relic platform.',
-    to: '/docs/apis/get-started/intro-apis/introduction-new-relic-apis',
-  },
-  {
-    title: 'Manage data',
-    description: 'Monitor and control your data usage.',
-    to: '/docs/manage-your-data',
-  },
-  {
-    title: 'Build on New Relic One',
-    description:
-      'Learn how to build custom apps on our platform at developer.newrelic.com',
-    to: 'https://developer.newrelic.com',
-  },
-];
-
-const fsoGuides = [
-  {
-    title: 'Introduction to Full-Stack Observability',
-    description:
-      'Get deep insight into everything from infrastructure to server code to end-user apps.',
-    to: '/docs/introduction-full-stack-observability',
-  },
-  {
-    title: 'APM',
-    description:
-      "Get real-time and trending data about your app's performance and stability.",
-    to: '/docs/apm',
-  },
-  {
-    title: 'Browser',
-    description:
-      'Measure website performance, track errors, and see how users interact with your site.',
-    to: '/docs/browser',
-  },
-  {
-    title: 'Distributed Tracing',
-    description:
-      'Track requests through your distributed system to find trends and anomalies.',
-    to:
-      '/docs/understand-dependencies/distributed-tracing/get-started/introduction-distributed-tracing',
-  },
-  {
-    title: 'Infrastructure',
-    description:
-      'Monitor your infrastructure—hosts, cloud providers, container services, backend services, orchestrators, and more.',
-    to: '/docs/infrastructure',
-  },
-  {
-    title: 'Logs in context',
-    description:
-      'Link your log data to APM agent data so you can pinpoint where things are going wrong.',
-    to:
-      '/docs/logs/enable-logs/configure-logs-context/configure-logs-context-apm-agents',
-  },
-  {
-    title: 'Mobile',
-    description:
-      'Understand user journeys in your Android and iOS apps and troubleshoot crashes.',
-    to: '/docs/mobile-monitoring',
-  },
-  {
-    title: 'Serverless',
-    description:
-      'Monitor AWS Lambda, Azure Functions, and Google Cloud Functions.',
-    to: '/docs/serverless-function-monitoring',
-  },
-  {
-    title: 'Synthetics',
-    description:
-      'Simulate user activity to detect outages and fix poor website performance.',
-    to: '/docs/synthetics',
-  },
-];
-
-const aiGuides = [
-  {
-    title: 'Introduction to Alerts',
-    description:
-      'Get notified about important changes in your system based on any data you connect to New Relic.',
-    to:
-      '/docs/alerts/new-relic-alerts/getting-started/introduction-new-relic-alerts',
-  },
-  {
-    title: 'Introduction to Applied Intelligence',
-    description:
-      'Recognize issues sooner, resolve problems faster, and reduce noise for better incident management.',
-    to:
-      '/docs/new-relic-one/use-new-relic-one/new-relic-ai/introduction-new-relic-ai',
-  },
-  {
-    title: 'Incident Intelligence',
-    description:
-      'Get an overview of all your incidents. See sources and related events, and find out how they all correlate.',
-    to:
-      '/docs/new-relic-one/use-new-relic-one/new-relic-ai/get-started-incident-intelligence',
-  },
-  {
-    title: 'Incident Workflows',
-    description:
-      'Enrich your incidents with New Relic data before sending them to your notification platform.',
-    to:
-      '/docs/new-relic-one/use-new-relic-one/new-relic-ai/enhance-notifications-using-incident-workflows',
-  },
-  {
-    title: 'Proactive Detection',
-    description: 'Get notified by Slack or webhook of unusual app behavior.',
-    to:
-      '/docs/new-relic-one/use-new-relic-one/new-relic-ai/proactive-detection-new-relic-ai',
-  },
-];
-
-const securityGuides = [
-  {
-    title: 'Data privacy',
-    description:
-      'New Relic takes your data privacy seriously. Our principles-based approach aims to go beyond the legal requirements for consent.',
-    to: '/docs/security/security-privacy/data-privacy',
-  },
-  {
-    title: 'Security compliance',
-    description:
-      'Whether your data is in transit to New Relic or at rest in our storage, strong encryption measures can help protect against unauthorized access or theft of valuable data.',
-    to: '/docs/security/security-privacy/compliance',
-  },
-  {
-    title: 'Security bulletins',
-    description:
-      'Security bulletins are a way for us to let you know about security vulnerabilities, remediation strategies, and applicable updates. ',
-    to: '/docs/security/new-relic-security/security-bulletins',
-  },
-  {
-    title: 'Licenses',
-    description:
-      "All about New Relic's licenses, Usage Plan, and policies. Find out what open source software we use in our products.",
-    to: '/docs/licenses',
-  },
-];
 
 const Section = ({ alternate, layout, ...props }) => {
   return (
@@ -680,39 +498,38 @@ WelcomeTile.propTypes = {
   to: PropTypes.string,
 };
 
-const DocTileGrid = ({ children, maxTiles }) => (
-  <div
-    css={css`
-      display: grid;
-      grid-template-columns: repeat(${maxTiles}, 1fr);
-      grid-gap: 1rem;
+const DocTileGrid = ({ children }) => {
+  const maxTiles = Math.min(Children.count(children), 5);
 
-      @media screen and (max-width: 1470px) {
-        grid-template-columns: repeat(3, 1fr);
-      }
+  return (
+    <div
+      css={css`
+        display: grid;
+        grid-template-columns: repeat(${maxTiles}, 1fr);
+        grid-gap: 1rem;
 
-      @media screen and (max-width: 700px) {
-        grid-template-columns: 1fr;
-      }
-    `}
-  >
-    {children}
-  </div>
-);
+        @media screen and (max-width: 1470px) {
+          grid-template-columns: repeat(3, 1fr);
+        }
+
+        @media screen and (max-width: 700px) {
+          grid-template-columns: 1fr;
+        }
+      `}
+    >
+      {children}
+    </div>
+  );
+};
 
 DocTileGrid.propTypes = {
   children: PropTypes.node,
-  maxTiles: PropTypes.number,
 };
 
-DocTileGrid.defaultProps = {
-  maxTiles: 5,
-};
-
-const DocTile = ({ guide: { title, description, to } }) => (
+const DocTile = ({ guide: { title, description, link } }) => (
   <SurfaceLink
     base={Surface.BASE.SECONDARY}
-    to={to}
+    to={link}
     css={css`
       color: currentColor;
       padding: 1rem;
@@ -743,7 +560,7 @@ DocTile.propTypes = {
   guide: PropTypes.shape({
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
-    to: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
   }).isRequired,
 };
 
