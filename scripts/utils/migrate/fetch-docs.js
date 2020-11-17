88682d203ec8f68647ea1673df4d21d487141f8f
Jerel Miller <jmiller@newrelic.com>
2020-11-17 15:19:54 -0700

    chore: add back attribute def type but ignore it when fetching docs

--- scripts/utils/migrate/fetch-docs.js
+++ scripts/utils/migrate/fetch-docs.js
@@ -4,12 +4,16 @@ const { TYPES, ITEMS_PER_TYPE, DIRECT_IDS } = require('../constants');
 
 const prop = (name) => (obj) => obj[name];
 
+const IGNORED_TYPES = [TYPES.ATTRIBUTE_DEFINITION];
+
 const fetchDocs = async () => {
-  const requests = Object.values(TYPES).map((type) =>
-    docsApi.get(path.join('/api/migration/content', type, 'list'), {
-      perPage: ITEMS_PER_TYPE[type],
-    })
-  );
+  const requests = Object.values(TYPES)
+    .filter((type) => !IGNORED_TYPES.includes(type))
+    .map((type) =>
+      docsApi.get(path.join('/api/migration/content', type, 'list'), {
+        perPage: ITEMS_PER_TYPE[type],
+      })
+    );
 
   const hardCodedRequests = DIRECT_IDS.map((id) =>
     docsApi.get(`/api/migration/content/page/${id}`)
