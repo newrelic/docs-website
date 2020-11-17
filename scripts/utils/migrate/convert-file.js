400d873e5f9328b6628d42d0a32e64530e3cc4d3
Jerel Miller <jmiller@newrelic.com>
2020-11-17 16:54:08 -0700

    chore: remove unused to json converter

--- scripts/utils/migrate/convert-file.js
+++ scripts/utils/migrate/convert-file.js
@@ -1,11 +1,9 @@
 const toMDX = require('./converters/to-mdx');
 const toMarkdown = require('./converters/to-markdown');
-const toJSON = require('./converters/to-json');
 
 const converters = {
   '.mdx': toMDX,
   '.md': toMarkdown,
-  '.json': toJSON,
 };
 
 const convertFile = (file) => converters[file.extname](file);
