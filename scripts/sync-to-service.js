3e510d231ea1a8b6b3be5f59402adf53ac214a0e
Jerel Miller <jmiller@newrelic.com>
2020-12-07 16:09:37 -0800

    feat: add a script to sync the docs data dictionary

---
+++ scripts/sync-to-service.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+const fetch = require('node-fetch');
+
+const env = process.env.ENV;
+
+const HOSTS = {
+  staging: 'https://data-dictionary.staging-service.newrelic.com',
+  production: 'https://data-dictionary.service.newrelic.com',
+  'eu-production': 'https://data-dictionary.service.newrelic.com',
+};
+
+const sync = async () => {
+  const res = await fetch(`${HOSTS[env]}/sync`, {
+    method: 'POST',
+    headers: {
+      'x-auth-key': process.env.AUTH_KEY,
+    },
+  });
+
+  const body = await res.text();
+
+  if (!res.ok) {
+    console.log(`Error: ${res.status}`);
+    console.log(body);
+
+    process.exit(1);
+  }
+};
+
+sync();
