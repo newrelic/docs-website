77c42119fb107251e2f29769610ba06194ccd0ca
Jerel Miller <jmiller@newrelic.com>
2020-12-08 18:32:36 -0800

    feat: migrate whats new as .md files

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -2,6 +2,7 @@ const fetchDocs = require('./utils/migrate/fetch-docs');
 const fetchDocCount = require('./utils/migrate/fetch-doc-count');
 const fetchAttributeDefinitions = require('./utils/migrate/fetch-attribute-definitions');
 const fetchEventDefinitions = require('./utils/migrate/fetch-event-definitions');
+const fetchWhatsNew = require('./utils/migrate/fetch-whats-new');
 const createDirectories = require('./utils/migrate/create-directories');
 const convertFile = require('./utils/migrate/convert-file');
 const createIndexPages = require('./utils/migrate/create-index-pages');
@@ -14,8 +15,14 @@ const createRawHTMLFiles = require('./utils/migrate/create-raw-html-files');
 const migrateNavStructure = require('./utils/migrate/migrate-nav-structure');
 const reporter = require('vfile-reporter');
 const rimraf = require('rimraf');
+const path = require('path');
 const { prop } = require('./utils/functional');
-const { NAV_DIR, CONTENT_DIR, DICTIONARY_DIR } = require('./utils/constants');
+const {
+  NAV_DIR,
+  CONTENT_DIR,
+  DICTIONARY_DIR,
+  WHATS_NEW_DIR,
+} = require('./utils/constants');
 
 const all = (list, fn) => Promise.all(list.map(fn));
 
@@ -32,6 +39,7 @@ const run = async () => {
     const docs = await fetchDocs();
     const attributeDefs = await fetchAttributeDefinitions();
     const eventDefs = await fetchEventDefinitions();
+    const whatsNew = await fetchWhatsNew();
     const attributeDefFiles = await all(attributeDefs, (doc) =>
       toVFile(doc, {
         baseDir: DICTIONARY_DIR,
@@ -46,17 +54,30 @@ const run = async () => {
         dirname: ({ name }) => `/events/${name}`,
       })
     );
+    const whatsNewFiles = await all(whatsNew, (doc) =>
+      toVFile(doc, {
+        baseDir: WHATS_NEW_DIR,
+        dirname: ({ releaseDateTime }) => {
+          const date = releaseDateTime.split(' ')[0];
+          const [year, month] = date.split('-');
+
+          return path.join(year, month);
+        },
+      })
+    );
     const definitionFiles = attributeDefFiles.concat(eventDefFiles);
     const files = await all(docs, toVFile).then((files) =>
       files.sort((a, b) => a.path.localeCompare(b.path))
     );
-    await fetchDocCount(files.concat(definitionFiles).length);
+    await fetchDocCount(
+      files.concat(definitionFiles).concat(whatsNewFiles).length
+    );
 
     logger.normal('Creating directories');
-    createDirectories(files.concat(definitionFiles));
+    createDirectories(files.concat(definitionFiles, whatsNewFiles));
 
     logger.normal('Converting files');
-    await all(files.concat(definitionFiles), convertFile);
+    await all(files.concat(definitionFiles, whatsNewFiles), convertFile);
 
     logger.normal('Running codemods on .mdx files');
     await all(
@@ -77,8 +98,9 @@ const run = async () => {
     const navFiles = migrateNavStructure(createNavStructure(files));
 
     logger.normal('Saving changes to files');
-    await all(files.concat(indexFiles, navFiles, definitionFiles), (file) =>
-      write(file, 'utf-8')
+    await all(
+      files.concat(indexFiles, navFiles, definitionFiles, whatsNewFiles),
+      (file) => write(file, 'utf-8')
     );
 
     // Run `DEBUG=true yarn migrate` to also write a `.html` file right next to
@@ -87,7 +109,7 @@ const run = async () => {
     // files should not be committed.
     if (process.env.DEBUG) {
       logger.normal('[DEBUG] Creating raw HTML files');
-      createRawHTMLFiles(files);
+      createRawHTMLFiles(files.concat(whatsNewFiles));
     }
 
     console.error(reporter(files.concat(navFiles), { quiet: true }));
