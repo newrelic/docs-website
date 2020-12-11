d0d9f96bc4dd20cc0bfe8fff8d5406f85dafb395
Jerel Miller <jmiller@newrelic.com>
2020-12-11 00:44:56 -0800

    fix: extract markdown styles into a common component

---
+++ src/components/MarkdownContainer.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
+import { useLayout } from '@newrelic/gatsby-theme-newrelic';
+
+const MarkdownContainer = ({ children, className }) => {
+  const { contentPadding } = useLayout();
+
+  return (
+    <div
+      className={className}
+      css={css`
+        > *:first-child {
+          margin-top: 0;
+        }
+
+        code {
+          padding: 0.125rem;
+          border-radius: 2px;
+        }
+
+        :not(pre) > code {
+          background: var(--tertiary-background-color);
+        }
+
+        p:last-child {
+          margin-bottom: 0;
+        }
+
+        p,
+        ol,
+        ul {
+          margin-bottom: 1.5rem;
+
+          &:last-child {
+            margin-bottom: 0;
+          }
+        }
+
+        ol,
+        ul {
+          margin-top: 0;
+          padding-left: 1rem;
+        }
+
+        h1,
+        h2,
+        h3 {
+          margin-bottom: 1rem;
+        }
+
+        h1,
+        h2 {
+          font-weight: bold;
+
+          &:not(:first-child) {
+            margin-top: 2rem;
+          }
+        }
+
+        h3,
+        h4 {
+          margin-top: 1rem;
+          font-weight: bold;
+        }
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          scroll-margin-top: calc(
+            var(--global-header-height) + ${contentPadding}
+          );
+        }
+
+        li:not(:last-child) {
+          margin-bottom: 0.75rem;
+        }
+
+        ul li ul {
+          margin-top: 1rem;
+          line-height: 1;
+        }
+      `}
+    >
+      {children}
+    </div>
+  );
+};
+
+MarkdownContainer.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+
+export default MarkdownContainer;
