31fbe0879db1d4c40444cd641dffaabcacba987e
Jerel Miller <jmiller@newrelic.com>
2020-12-11 00:12:06 -0800

    chore: better sizing of the wistia embed

--- plugins/gatsby-remark-videos/index.js
+++ plugins/gatsby-remark-videos/index.js
@@ -1,9 +1,7 @@
 const visit = require('unist-util-visit');
 const { parse } = require('node-html-parser');
 
-module.exports = ({ markdownAST }, pluginOptions) => {
-  const { maxWidth } = pluginOptions;
-
+module.exports = ({ markdownAST }) => {
   visit(markdownAST, 'html', (node) => {
     const root = parse(node.value);
     const element = root.firstChild;
@@ -16,12 +14,8 @@ module.exports = ({ markdownAST }, pluginOptions) => {
     const title = element.getAttribute('title');
 
     node.value = `
-<div class="wistia_responsive_padding" style="padding: 56.25% 0 0 0; position: relative; max-width: ${
-      maxWidth ? `${maxWidth}px` : 'none'
-    };">
-  <div class="wistia_responsive_wrapper" style="height: 100%; width: 100%; position: absolute; left: 0; top: 0;">
-    <iframe allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen src="${src}" title="${title}" class="wistia_embed" frameborder="0" />
-  </div>
+<div class="wistia_responsive_wrapper" style="padding: 56.25% 0 0 0; position: relative; height: 0;">
+  <iframe allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen src="${src}" title="${title}" class="wistia_embed" frameborder="0" style="position: absolute; top: 0; left: 0; height: 100%; width: 100%;" />
 </div>
     `.trim();
   });
