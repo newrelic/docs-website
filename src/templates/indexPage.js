7e27d159376e901fc3dce925720ceb514927536f
Jerel Miller <jmiller@newrelic.com>
2020-12-15 22:16:55 -0800

    feat: render nav on index page if its available

--- src/templates/indexPage.js
+++ src/templates/indexPage.js
@@ -4,23 +4,33 @@ import { Layout } from '@newrelic/gatsby-theme-newrelic';
 import { graphql } from 'gatsby';
 import PageTitle from '../components/PageTitle';
 import SEO from '../components/seo';
+import IndexContents from '../components/IndexContents';
 import TableOfContentsContainer from '../components/TableOfContentsContainer';
 
-const IndexPage = ({ pageContext }) => {
-  const { html, title } = pageContext;
+const IndexPage = ({ data, pageContext }) => {
+  const { nav } = data;
+  const { html } = pageContext;
+  const title = nav ? nav.title : pageContext.title;
 
   return (
     <>
       <SEO title={title} />
       <PageTitle>{title}</PageTitle>
       <Layout.Content>
-        <TableOfContentsContainer dangerouslySetInnerHTML={{ __html: html }} />
+        {nav ? (
+          <IndexContents nav={nav} />
+        ) : (
+          <TableOfContentsContainer
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        )}
       </Layout.Content>
     </>
   );
 };
 
 IndexPage.propTypes = {
+  data: PropTypes.object.isRequired,
   pageContext: PropTypes.object.isRequired,
 };
 
