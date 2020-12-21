a33e44dc1cf35412d5af613a7ba97d07f46b3d7f
Clinton <clinton.langosch@gmail.com>
2020-12-21 11:40:23 -0800

    refactor: use template and index.mdx instead of local plugin

--- plugins/gatsby-plugin-auto-index-pages/gatsby-node.js
+++ plugins/gatsby-plugin-auto-index-pages/gatsby-node.js
@@ -18,6 +18,9 @@ exports.createPages = async ({ actions, graphql, reporter }, pluginOptions) => {
       ) {
         nodes {
           childMdx {
+            frontmatter {
+              contentType
+            }
             fields {
               slug
             }
@@ -110,6 +113,12 @@ exports.createPages = async ({ actions, graphql, reporter }, pluginOptions) => {
   });
 
   tableOfContentsNodes.forEach((node) => {
+    if (
+      !node.childMdx ||
+      node.childMdx.frontmatter.contentType !== 'landingPage'
+    ) {
+      return;
+    }
     const landingPageSlug = getSlug(node);
     const slug = path.join(landingPageSlug, 'table-of-contents');
 
