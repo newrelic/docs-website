acfca6335d5c103726add506fd05c5f26a19b860
Jerel Miller <jmiller@newrelic.com>
2021-01-07 14:35:03 -0800

    chore: rename the type for nav json

--- gatsby-config.js
+++ gatsby-config.js
@@ -138,6 +138,13 @@ module.exports = {
     },
     {
       resolve: 'gatsby-transformer-json',
+      options: {
+        // If we need to source json files other than the i18n/nav, we should
+        // consider making this dynamic. See the docs for ways to do this.
+        //
+        // https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-transformer-json
+        typeName: 'TranslatedNavJson'
+      }
     },
     {
       resolve: 'gatsby-transformer-remark',
