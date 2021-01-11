45fdab8e5b6d13140689288702b49c63355a6951
Clinton <clinton.langosch@gmail.com>
2021-01-11 15:04:27 -0800

    refactor: remove ul and pass in as parent prop to Trans

--- src/components/DefaultRelatedContent.js
+++ src/components/DefaultRelatedContent.js
@@ -23,35 +23,33 @@ const DefaultRelatedContent = () => {
         {t('defaultRelatedContent.title')}
       </h2>
       <p>{t('defaultRelatedContent.intro')}</p>
-      <ul>
-        <Trans i18nKey="defaultRelatedContent.list">
-          <li>
-            Browse the{' '}
-            <ExternalLink href="https://discuss.newrelic.com">
-              Explorers Hub
-            </ExternalLink>{' '}
-            to get help from the community and join in discussions.
-          </li>
-          <li>
-            Find{' '}
-            <a href="/docs/using-new-relic/welcome-new-relic/get-started/find-help-use-support-portal">
-              answers on our sites and learn how to use our support portal
-            </a>
-            .
-          </li>
-          <li>
-            Run{' '}
-            <a href="/docs/using-new-relic/cross-product-functions/troubleshooting/new-relic-diagnostics">
-              New Relic Diagnostics
-            </a>
-            , our troubleshooting tool for Linux, Windows, and macOS.
-          </li>
-          <li>
-            Review New Relic's <a href="/docs/security">data security</a> and{' '}
-            <a href="/docs/licenses">licenses</a> documentation.
-          </li>
-        </Trans>
-      </ul>
+      <Trans i18nKey="defaultRelatedContent.list" parent="ul">
+        <li>
+          Browse the{' '}
+          <ExternalLink href="https://discuss.newrelic.com">
+            Explorers Hub
+          </ExternalLink>{' '}
+          to get help from the community and join in discussions.
+        </li>
+        <li>
+          Find{' '}
+          <a href="/docs/using-new-relic/welcome-new-relic/get-started/find-help-use-support-portal">
+            answers on our sites and learn how to use our support portal
+          </a>
+          .
+        </li>
+        <li>
+          Run{' '}
+          <a href="/docs/using-new-relic/cross-product-functions/troubleshooting/new-relic-diagnostics">
+            New Relic Diagnostics
+          </a>
+          , our troubleshooting tool for Linux, Windows, and macOS.
+        </li>
+        <li>
+          Review New Relic's <a href="/docs/security">data security</a> and{' '}
+          <a href="/docs/licenses">licenses</a> documentation.
+        </li>
+      </Trans>
     </div>
   );
 };
