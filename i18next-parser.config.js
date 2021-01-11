5c2f3c31590cf16249638ba5ebce3c2dd131a918
Clinton <clinton.langosch@gmail.com>
2021-01-11 15:02:43 -0800

    chore: remove unnecessary pattern match for Trans component

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -5,5 +5,5 @@ module.exports = {
   defaultNamespace: 'translation',
   locales: ['en', 'jp'],
   output: './src/i18n/translations/$LOCALE/translation.json',
-  input: ['src/**/!(Trans)*.js'],
+  input: ['src/**/*.js'],
 };
