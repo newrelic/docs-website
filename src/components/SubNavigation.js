7aa7c66df31958f8756a1d85f36ca2295efdd9f2
Clinton <clinton.langosch@gmail.com>
2021-01-11 12:46:23 -0800

    feat: add en translation for SubNavigation component

--- src/components/SubNavigation.js
+++ src/components/SubNavigation.js
@@ -2,46 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 import { Link } from 'gatsby';
-import { NavItem, Icon } from '@newrelic/gatsby-theme-newrelic';
+import { NavItem, Icon, useTranslation } from '@newrelic/gatsby-theme-newrelic';
 
-const SubNavigation = ({ nav }) => (
-  <>
-    <Link
-      to="/"
-      css={css`
-        color: var(--primary-text-color);
-        display: flex;
-        align-items: center;
-        padding: 0.5rem 0;
-        margin-bottom: 0.5rem;
-        transition: 0.2s ease-out;
-
-        &:hover {
-          color: var(--primary-text-hover-color);
-        }
-      `}
-    >
-      <Icon
-        name="fe-home"
-        size="1rem"
+const SubNavigation = ({ nav }) => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <Link
+        to="/"
         css={css`
-          margin-right: 0.5rem;
+          color: var(--primary-text-color);
+          display: flex;
+          align-items: center;
+          padding: 0.5rem 0;
+          margin-bottom: 0.5rem;
+          transition: 0.2s ease-out;
+
+          &:hover {
+            color: var(--primary-text-hover-color);
+          }
         `}
-      />
-      Home
-    </Link>
-    {nav && (
-      <>
-        <h2>{nav.title}</h2>
-        <nav role="navigation" aria-label="SubNavigation">
-          {nav.pages.map((page) => (
-            <NavItem key={page.title} page={page} />
-          ))}
-        </nav>
-      </>
-    )}
-  </>
-);
+      >
+        <Icon
+          name="fe-home"
+          size="1rem"
+          css={css`
+            margin-right: 0.5rem;
+          `}
+        />
+        {t('subNav.title')}
+      </Link>
+      {nav && (
+        <>
+          <h2>{nav.title}</h2>
+          <nav role="navigation" aria-label="SubNavigation">
+            {nav.pages.map((page) => (
+              <NavItem key={page.title} page={page} />
+            ))}
+          </nav>
+        </>
+      )}
+    </>
+  );
+};
 
 SubNavigation.propTypes = {
   nav: PropTypes.object,
