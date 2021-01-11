c87705feb63068aedf49d3410dee616c9758066d
Clinton <clinton.langosch@gmail.com>
2021-01-11 11:17:58 -0800

    feat: add en and jp translations for defaultRelatedContent

--- src/components/DefaultRelatedContent.js
+++ src/components/DefaultRelatedContent.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { Link, Icon, ExternalLink } from '@newrelic/gatsby-theme-newrelic';
+import {
+  Link,
+  Icon,
+  ExternalLink,
+  useTranslation,
+  Trans,
+} from '@newrelic/gatsby-theme-newrelic';
 
 const DefaultRelatedContent = () => {
+  const { t } = useTranslation();
+
   return (
     <div data-swiftype-index={false}>
       <h2 id="for-more-help" style={{ position: 'relative' }}>
@@ -12,37 +20,37 @@ const DefaultRelatedContent = () => {
         >
           <Icon focusable={false} name="fe-link-2" size="1rem" />
         </Link>
-        For more help
+        {t('defaultRelatedContent.title')}
       </h2>
-      <p>
-        If you need more help, check out these support and learning resources:
-      </p>
+      <p>{t('defaultRelatedContent.intro')}</p>
       <ul>
-        <li>
-          Browse the{' '}
-          <ExternalLink href="https://discuss.newrelic.com">
-            Explorers Hub
-          </ExternalLink>{' '}
-          to get help from the community and join in discussions.
-        </li>
-        <li>
-          Find{' '}
-          <a href="/docs/using-new-relic/welcome-new-relic/get-started/find-help-use-support-portal">
-            answers on our sites and learn how to use our support portal
-          </a>
-          .
-        </li>
-        <li>
-          Run{' '}
-          <a href="/docs/using-new-relic/cross-product-functions/troubleshooting/new-relic-diagnostics">
-            New Relic Diagnostics
-          </a>
-          , our troubleshooting tool for Linux, Windows, and macOS.
-        </li>
-        <li>
-          Review New Relic's <a href="/docs/security">data security</a> and{' '}
-          <a href="/docs/licenses">licenses</a> documentation.
-        </li>
+        <Trans i18nKey="defaultRelatedContent.list">
+          <li>
+            Browse the{' '}
+            <ExternalLink href="https://discuss.newrelic.com">
+              Explorers Hub
+            </ExternalLink>{' '}
+            to get help from the community and join in discussions.
+          </li>
+          <li>
+            Find{' '}
+            <a href="/docs/using-new-relic/welcome-new-relic/get-started/find-help-use-support-portal">
+              answers on our sites and learn how to use our support portal
+            </a>
+            .
+          </li>
+          <li>
+            Run{' '}
+            <a href="/docs/using-new-relic/cross-product-functions/troubleshooting/new-relic-diagnostics">
+              New Relic Diagnostics
+            </a>
+            , our troubleshooting tool for Linux, Windows, and macOS.
+          </li>
+          <li>
+            Review New Relic's <a href="/docs/security">data security</a> and{' '}
+            <a href="/docs/licenses">licenses</a> documentation.
+          </li>
+        </Trans>
       </ul>
     </div>
   );
