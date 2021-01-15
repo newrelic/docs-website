f559158f8c54e59753f554cfbe6fd11e806324e1
Jerel Miller <jmiller@newrelic.com>
2021-01-15 12:19:17 -0800

    chore: add additional deleted content

--- scripts/utils/migrate/copy-manual-edits.js
+++ scripts/utils/migrate/copy-manual-edits.js
@@ -3,10 +3,11 @@ const vfileGlob = require('vfile-glob');
 const { write } = require('to-vfile');
 const rimraf = require('rimraf');
 
-
 const deletedFiles = [
-  'src/content/docs/new-relic-only/tech-writer-style-guide/processes-procedures/clear-cache.mdx'
-]
+  'src/content/docs/new-relic-only/tech-writer-style-guide/processes-procedures/clear-cache.mdx',
+  'src/content/docs/new-relic-only/basic-style-guide/style-guide-quick-reference/html-cheat-sheet.mdx',
+  'src/content/docs/new-relic-only/basic-style-guide/style-guide-quick-reference/lists.mdx',
+];
 
 const copyManualEdits = () => {
   return new Promise((resolve) => {
@@ -21,7 +22,7 @@ const copyManualEdits = () => {
         await write(file, 'utf-8');
       },
       complete: () => {
-        deletedFiles.forEach(file => rimraf.sync(file))
+        deletedFiles.forEach((file) => rimraf.sync(file));
         resolve();
       },
     });
