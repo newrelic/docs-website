26f930a93b79d5ff0a878c92fc09eca34439cc21
Jerel Miller <jmiller@newrelic.com>
2021-01-19 10:38:51 -0800

    chore: reorganize how serializers work

--- codemods/serialize/componentsToData.js
+++ codemods/serialize/componentsToData.js
@@ -2,29 +2,51 @@ const visit = require('unist-util-visit');
 const { isMdxElement } = require('../utils/mdxast');
 const serializeComponent = require('./serializeComponent');
 
-const COMPONENTS = [
-  'Button',
-  'ButtonLink',
-  'Callout',
-  'CollapserGroup',
-  'ExternalLink',
-  'Link',
-  'Icon',
-  'DefaultRelatedContent',
-  'Table',
-  'LandingPageTitle',
-  'LandingPageTitleGrid',
-  'inlineCode',
-];
+const handlers = {
+  Button: {
+    serialize: (node) => serializeComponent(node),
+  },
+  ButtonLink: {
+    serialize: (node) => serializeComponent(node),
+  },
+  Callout: {
+    serialize: (node) => serializeComponent(node),
+  },
+  CollapserGroup: {
+    serialize: (node) => serializeComponent(node),
+  },
+  ExternalLink: {
+    serialize: (node) => serializeComponent(node),
+  },
+  Link: {
+    serialize: (node) => serializeComponent(node),
+  },
+  Icon: {
+    serialize: (node) => serializeComponent(node),
+  },
+  Table: {
+    serialize: (node) => serializeComponent(node),
+  },
+  LandingPageTitle: {
+    serialize: (node) => serializeComponent(node),
+  },
+  LandingPageTitleGrid: {
+    serialize: (node) => serializeComponent(node),
+  },
+  InlineCode: {
+    serialize: (node) => serializeComponent(node),
+  },
+};
 
 const componentsToData = () => (tree) => {
   visit(
     tree,
-    (node) => {
-      return COMPONENTS.some((component) => isMdxElement(component, node));
-    },
-    (component) => {
-      serializeComponent(component);
+    (node) =>
+      Object.keys(handlers).some((componentName) =>
+        isMdxElement(componentName, node)
+      ),
+    (node, ...args) => {
+      handlers[node.name].serialize(node, ...args);
     }
   );
 };
