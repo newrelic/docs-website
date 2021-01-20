b0d811cb9c1fc21fecfc6d66e032c6e334142b1a
Jerel Miller <jmiller@newrelic.com>
2021-01-19 16:16:09 -0800

    chore: create a test for deserializing html

---
+++ scripts/actions/__tests__/deserialize-html.test.js
@@ -0,0 +1,95 @@
+import deserializeHTML from '../deserialize-html';
+import serializeMDX from '../serialize-mdx';
+import fs from 'fs';
+
+test('deserializes mdx with frontmatter', async () => {
+  const input = `
+---
+title: The doc
+---
+
+This is an MDX doc
+  `;
+
+  const mdx = await deserializeHTML(await serializeMDX(input));
+
+  expect(mdx).toEqual(input);
+});
+
+test('deserializes mdx with import statements', async () => {
+  const input = `
+import { Link } from '@newrelic/gatsby-theme-newrelic'
+
+This is an MDX doc
+  `;
+
+  const mdx = await deserializeHTML(await serializeMDX(input));
+
+  expect(mdx).toEqual(input);
+});
+
+test('deserializes simple components', async () => {
+  const input = `
+<Button
+  to="/docs/agents/ruby-sdk"
+>
+  Go to Ruby docs
+</Button>
+  `;
+
+  const mdx = await deserializeHTML(await serializeMDX(input));
+
+  expect(mdx).toEqual(input);
+});
+
+test('deserializes components with text props', async () => {
+  const input = `
+<Callout
+  title="Life tip #3"
+>
+  There are 10 types of people in this world. Those that understand binary and those that don't.
+</Callout>
+  `;
+
+  const mdx = await deserializeHTML(await serializeMDX(input));
+
+  expect(mdx).toEqual(input);
+});
+
+test('deserializes components with text props as JSX expressions', async () => {
+  const input = `
+<Collapser
+  title={<><InlineCode>agent.send_custom_event</InlineCode> API</>}
+>
+  All about that API
+</Callout>
+  `;
+
+  const mdx = await deserializeHTML(await serializeMDX(input));
+
+  expect(mdx).toEqual(input);
+});
+
+test('deserializes components with children', async () => {
+  const input = `
+<CollapserGroup>
+  <Collapser
+    title={<><InlineCode>agent.send_custom_event</InlineCode> API</>}
+  >
+    All about that API
+  </Callout>
+</CollapserGroup>
+  `;
+
+  const mdx = await deserializeHTML(await serializeMDX(input));
+
+  expect(mdx).toEqual(input);
+});
+
+test('kitchen sink', async () => {
+  const input = fs.readFileSync(`${__dirname}/kitchen-sink.mdx`, 'utf-8');
+
+  const mdx = await deserializeHTML(await serializeMDX(input));
+
+  expect(mdx).toEqual(input);
+});
