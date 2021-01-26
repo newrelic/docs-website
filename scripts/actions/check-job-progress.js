8b491da3947f7c49dedda4135a2dba67add15af7
Zack Stickles <zstickles@newrelic.com>
2021-01-25 17:32:45 -0800

    wip: getting information about batches

--- scripts/actions/check-job-progress.js
+++ scripts/actions/check-job-progress.js
@@ -1,31 +1,62 @@
+const loadFromDB = require('./utils/load-from-db');
 const fetchAndDeserialize = require('./fetch-and-deserialize');
-const { vendorRequest } = require('./utils/vendor-request');
+const { getAccessToken, vendorRequest } = require('./utils/vendor-request');
 
-const checkJobStatus = async (batchUid) => {
-  const {
-    response: {
-      data: { status },
-    },
-  } = await vendorRequest(
-    'GET',
-    `https://api.smartling.com/jobs-api/v3/projects/${project}/jobs/${batchUid}`
-  );
+const PROJECT_ID = process.env.TRANSLATION_VENDOR_PROJECT;
 
-  return status === 'COMPLETED';
+/**
+ * @typedef {Object} BatchStatus
+ * @property {string} batchUid
+ * @property {string} locale The vendor-based locale (i.e. "ja-JP")
+ * @property {string[]} fileUris The filepath from the root of the project
+ * @property {boolean} done
+ */
+
+/**
+ * @param {string} accessToken
+ * @returns {(batchUid: string) => Promise<BatchStatus>}
+ */
+const getBatchStatus = (accessToken) => async (batchUid) => {
+  const resp = await vendorRequest({
+    method: 'GET',
+    endpoint: `/jobs-api/v3/projects/${PROJECT_ID}/jobs/${batchUid}`,
+    accessToken,
+  });
+
+  const { status, files } = resp.response.data;
+
+  return {
+    batchUid,
+    done: status === 'COMPLETED',
+    locale: files[0].targetLocales[0].localeId,
+    fileUris: files.map((file) => file.fileUri),
+  };
 };
 
 const main = async () => {
-  //get list of current job batch uids from dyanmoDB, for each one execute checkJobStatus
-  try {
-    ///const isCompleted = checkJobStatus(batchUid);
-    const isCompleted = true;
+  const table = 'TranslationQueues';
+  const key = { type: 'being_translated' };
 
-    if (isCompleted) {
-      await fetchAndDeserialize();
-      process.exit(0);
-    } else {
-      process.exit(1);
-    }
+  try {
+    const queue = await loadFromDB(table, key);
+    const { batchUids } = queue.Item;
+
+    const accessToken = await getAccessToken();
+
+    const batchesToDeserialize = await Promise.all(
+      batchUids.map(getBatchStatus(accessToken))
+    ).filter((batch) => batch && batch.done);
+    console.log(
+      `[*] ${batchesToDeserialize.length} batches ready to be deserialized`
+    );
+
+    await fetchAndDeserialize(batchesToDeserialize, accessToken);
+    console.log('[*] Content deserialized');
+
+    // TODO: update queue to remove batchUid
+    console.log('[*] "Being translated" queue updated');
+
+    process.exit(0);
   } catch (error) {
     console.error(`[!] Unable to check job status`);
     console.log(error);
--- scripts/actions/fetch-and-deserialize.js
+++ scripts/actions/fetch-and-deserialize.js
@@ -1,8 +1,9 @@
-const { vendorRequest, vendorGetRequest } = require('./utils/vendor-request');
 const AdmZip = require('adm-zip');
-const deserializedHtml = require('./deserialize-html');
-const { write } = require('to-vfile');
 const vfile = require('vfile');
+const { write } = require('to-vfile');
+
+const { vendorRequest } = require('./utils/vendor-request');
+const deserializedHtml = require('./deserialize-html');
 const createDirectories = require('../utils/migrate/create-directories');
 
 const localesMap = {
@@ -11,73 +12,56 @@ const localesMap = {
 
 const projectId = process.env.TRANSLATION_VENDOR_PROJECT;
 
-const fetchFileURIs = async (batchUid) => {
-  const {
-    response: {
-      data: { files },
-    },
-  } = await vendorRequest(
-    'GET',
-    `/job-batches-api/v2/projects/${projectId}/batches/${batchUid}`
-  );
-  return files.map((file) => file.fileUri);
-};
-
-const fetchTranslatedFilesZip = async (fileUris, locale) => {
+const fetchTranslatedFilesZip = async (fileUris, locale, accessToken) => {
   const fileUriStr = fileUris.reduce((str, uri) => {
     return str.concat(`&fileUris[]=${uri}`);
   }, '');
 
   const localeIdStr = `localeIds[]=${locale}`;
 
-  return vendorGetRequest(
-    `/files-api/v2/projects/${projectId}/files/zip?${localeIdStr}${fileUriStr}`
-  );
+  return vendorRequest({
+    method: 'GET',
+    endpoint: `/files-api/v2/projects/${projectId}/files/zip?${localeIdStr}${fileUriStr}`,
+    accessToken,
+  });
 };
 
-const fetchAndDeserialize = async () => {
-  try {
-    const locale = 'ja-JP';
-    const fileUris = ['enable-serverless-monitoring-aws-lambda.html'];
+const fetchAndDeserialize = (accessToken) => async ({ locale, fileUris }) => {
+  const response = await fetchTranslatedFilesZip(fileUris, locale, accessToken);
+  const buffer = await response.buffer();
 
-    const response = await fetchTranslatedFilesZip(fileUris, locale);
-    const buffer = await response.buffer();
+  const zip = new AdmZip(buffer);
+  const zipEntries = zip.getEntries();
 
-    const zip = new AdmZip(buffer);
-    const zipEntries = zip.getEntries();
+  const translatedHtml = zipEntries.map((entry) => {
+    return {
+      path: entry.entryName.split('/').pop().split('.').slice(0, -1), // get the last element from the folder
+      html: zip.readAsText(entry, (encoding = 'utf8')),
+    };
+  });
 
-    const translatedHtml = zipEntries.map((entry) => {
+  const deserializedMdx = Promise.all(
+    translatedHtml.map(async ({ path, html }) => {
       return {
-        path: entry.entryName.split('/').pop().split('.').slice(0, -1), //get the last element from the folder
-        html: zip.readAsText(entry, (encoding = 'utf8')),
+        path: `src/i18n/content/${localesMap[locale]}/docs/${path}`,
+        mdx: await deserializedHtml(html),
       };
-    });
+    })
+  );
 
-    const deserializedMdx = Promise.all(
-      translatedHtml.map(async ({ path, html }) => {
-        return {
-          path: `src/i18n/content/${localesMap[locale]}/docs/${path}`,
-          mdx: await deserializedHtml(html),
-        };
-      })
-    );
+  const files = (await deserializedMdx).map(
+    ({ path, mdx }) =>
+      vfile({
+        contents: mdx,
+        path,
+        extname: '.mdx',
+      }),
+    'utf-8'
+  );
 
-    const files = (await deserializedMdx).map(
-      ({ path, mdx }) =>
-        vfile({
-          contents: mdx,
-          path,
-          extname: '.mdx',
-        }),
-      'utf-8'
-    );
+  createDirectories(files);
 
-    createDirectories(files);
-
-    files.forEach((file) => write(file, 'utf-8'));
-  } catch (error) {
-    console.error(error);
-  }
+  files.forEach((file) => write(file, 'utf-8'));
 };
 
 fetchAndDeserialize();
