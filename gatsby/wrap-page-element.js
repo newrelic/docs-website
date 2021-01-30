5b336febc3e5e519cf4dd247378417c3f4b9b697
Jerel Miller <jmiller@newrelic.com>
2021-01-29 17:49:14 -0800

    chore: add var global styles

--- gatsby/wrap-page-element.js
+++ gatsby/wrap-page-element.js
@@ -9,6 +9,52 @@ const wrapPageElement = ({ element }) => (
         html {
           scroll-behavior: smooth;
         }
+
+        var,
+        mark {
+          line-height: 1.25;
+          display: inline-block;
+          padding: 0.125rem;
+          border-radius: 0.125rem;
+        }
+
+        var {
+          font-style: normal;
+          font-size: 85%;
+          transition: all 0.15s ease-out;
+          color: var(--color-neutrals-900);
+          background: var(--color-brand-100);
+
+          .dark-mode & {
+            color: var(--color-white);
+            background: var(--color-brand-600);
+          }
+        }
+
+        mark {
+          .dark-mode & {
+            background: var(--color-yellow-100);
+          }
+        }
+
+        a:hover var {
+          background: var(--color-brand-200);
+
+          .dark-mode & {
+            background: var(--color-brand-500);
+          }
+        }
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          var {
+            font-size: inherit;
+          }
+        }
       `}
     />
     {element}
