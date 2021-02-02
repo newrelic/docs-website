8fe3f12b54dc7e643c0064251f5fc2752dfab2f6
Cayla Hamann <chamann@newrelic.com>
2021-02-02 18:48:41 -0500

    chore: another test

--- scripts/actions/remove-completed-batch.js
+++ scripts/actions/remove-completed-batch.js
@@ -14,9 +14,9 @@ const saveRemainingBatches = async () => {
   checkArgs(4);
 
   const batchUids = JSON.parse(process.argv[2]);
-  const deserializedFileUris = process.argv[3];
+  const deserializedFileUris = JSON.parse(process.argv[3]);
 
-  console.log(deserializedFileUris);
+  console.log(deserializedFileUris, typeof deserializedFileUris);
 
   await removePageContext(deserializedFileUris);
 
