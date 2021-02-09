eb2df76e3c0a4aaf760f70e40b386b95a2f80282
Jerel Miller <jmiller@newrelic.com>
2021-02-09 11:58:01 -0800

    chore: move integration icons over to logo icons for use with Icon component

---
+++ src/@newrelic/gatsby-theme-newrelic/icons/logo.js
@@ -0,0 +1,75 @@
+import defaultIcons from '@newrelic/gatsby-theme-newrelic/src/icons/logo';
+import androidColor from './logo/android-color';
+import apache from './logo/apache';
+import appleColor from './logo/apple-color';
+import aws from './logo/aws';
+import azure from './logo/azure';
+import c from './logo/c';
+import dotnet from './logo/dotnet';
+import dropwizard from './logo/dropwizard';
+import gcloud from './logo/gcloud';
+import istio from './logo/istio';
+import java from './logo/java';
+import javascript from './logo/javascript';
+import jmx from './logo/jmx';
+import kafka from './logo/kafka';
+import kamon from './logo/kamon';
+import kubernetes from './logo/kubernetes';
+import go from './logo/go';
+import linux from './logo/linux';
+import micrometer from './logo/micrometer';
+import mongodb from './logo/mongodb';
+import mssql from './logo/mssql';
+import mysql from './logo/mysql';
+import newrelic from './logo/newrelic';
+import nginx from './logo/nginx';
+import nodejs from './logo/nodejs';
+import opencensus from './logo/opencensus';
+import opentelemetry from './logo/opentelemetry';
+import php from './logo/php';
+import postgresql from './logo/postgresql';
+import prometheus from './logo/prometheus';
+import python from './logo/python';
+import redis from './logo/redis';
+import ruby from './logo/ruby';
+import statsd from './logo/statsd';
+import windows from './logo/windows';
+
+export default {
+  ...defaultIcons,
+  'android-color': androidColor,
+  apache,
+  'apple-color': appleColor,
+  aws,
+  azure,
+  c,
+  dotnet,
+  dropwizard,
+  gcloud,
+  istio,
+  java,
+  javascript,
+  jmx,
+  kafka,
+  kamon,
+  kubernetes,
+  go,
+  linux,
+  micrometer,
+  mongodb,
+  mssql,
+  mysql,
+  newrelic,
+  nginx,
+  nodejs,
+  opencensus,
+  opentelemetry,
+  php,
+  postgresql,
+  prometheus,
+  python,
+  redis,
+  ruby,
+  statsd,
+  windows,
+};
