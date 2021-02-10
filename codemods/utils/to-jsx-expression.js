e366ad882a91215274f8bc59ac987cc0ecee93cb
Jerel Miller <jmiller@newrelic.com>
2021-02-10 13:25:49 -0800

    chore: add image and break handlers for to-jsx-expression

--- codemods/utils/to-jsx-expression.js
+++ codemods/utils/to-jsx-expression.js
@@ -1,25 +1,30 @@
 const {
   mdxAttribute,
+  mdxImport,
   mdxSpanElement,
   mdxSpanExpression,
   mdxValueExpression,
 } = require('./mdxast-builder');
+const { camelCase } = require('lodash');
+const path = require('path');
 
 const { isPlainText } = require('./mdxast');
 const { root, text } = require('mdast-builder');
 const stringify = require('./mdxast-stringify');
 
-const toJSXExpression = (node, file) => {
-  const children = transformChildren(node, file);
-  const tree = root(
+const toJSXExpression = (node, file, tree) => {
+  const children = transformChildren(node, file, tree);
+  const attributeNode = root(
     children.length === 1 ? children : [mdxSpanElement(null, [], children)]
   );
-  const expression = stringify(tree).trim();
+  const expression = stringify(attributeNode).trim();
 
-  return isPlainText(tree) ? expression : mdxValueExpression(expression);
+  return isPlainText(attributeNode)
+    ? expression
+    : mdxValueExpression(expression);
 };
 
-const transformChildren = (node, file) => {
+const transformChildren = (node, file, tree) => {
   return node.children
     .flatMap((child) => {
       const transformer = TRANSFORMERS[child.type];
@@ -33,7 +38,7 @@ const transformChildren = (node, file) => {
         return;
       }
 
-      return transformer(child, file);
+      return transformer(child, file, tree);
     })
     .filter(Boolean);
 };
@@ -62,6 +67,29 @@ const TRANSFORMERS = {
       isExternal ? 'ExternalLink' : 'Link',
       [mdxAttribute(isExternal ? 'href' : 'to', node.url)],
       transformChildren(node, file)
+    );
+  },
+  break: () => mdxSpanElement('br'),
+  image: (node, _file, tree) => {
+    const isRelativeImport = Boolean(node.url.match(/^\.\.?\//));
+    const importName = camelCase(
+      path.basename(node.url, path.extname(node.url))
+    );
+
+    if (isRelativeImport) {
+      tree.children.splice(1, 0, mdxImport(importName, node.url));
+    }
+
+    return mdxSpanElement(
+      'img',
+      [
+        mdxAttribute(
+          'src',
+          isRelativeImport ? mdxValueExpression(importName) : node.url
+        ),
+        node.title && mdxAttribute('title', node.title),
+        node.alt && mdxAttribute('alt', node.alt),
+      ].filter(Boolean)
     );
   },
   mdxSpanElement: (node) => node,
