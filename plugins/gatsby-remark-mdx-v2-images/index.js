1b57f2091404cab94d860530b0edb3c12ce6f3c7
Jerel Miller <jmiller@newrelic.com>
2021-02-09 22:12:45 -0800

    feat: add styles for inline images

--- plugins/gatsby-remark-mdx-v2-images/index.js
+++ plugins/gatsby-remark-mdx-v2-images/index.js
@@ -2,14 +2,41 @@ const visit = require('unist-util-visit');
 const { JSDOM } = require('jsdom');
 const fromDom = require('hast-util-from-dom');
 
+const parseStyleString = (string) =>
+  string
+    .split(/;\s*/)
+    .filter(Boolean)
+    .reduce((style, rule) => {
+      const [property, value] = rule.split(/:\s*/);
+
+      return { ...style, [property]: value };
+    }, {});
+
+const compileStyleObject = (styles) =>
+  Object.entries(styles)
+    .map((rule) => rule.join(': '))
+    .join('; ');
+
 module.exports = ({ markdownAST }) => {
-  visit(markdownAST, 'html', (node) => {
+  visit(markdownAST, 'html', (node, _idx, parent) => {
     if (!matchesGatsbyImage(node.value)) {
       return;
     }
 
+    const isBlockImage =
+      parent.type === 'paragraph' && parent.children.length === 1;
+
     const { document } = new JSDOM(node.value).window;
     const tree = fromDom(document.body.firstChild);
+
+    if (!isBlockImage) {
+      const style = parseStyleString(tree.properties.style);
+
+      style.display = 'inline-block';
+      style.width = '100%';
+
+      tree.properties.style = compileStyleObject(style);
+    }
 
     node.type = 'jsx';
     node.data = {
