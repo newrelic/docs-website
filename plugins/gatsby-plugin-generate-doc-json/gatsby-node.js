2eeb2932c43b54da5140b5d052b094949723c070
Cayla Hamann <chamann@newrelic.com>
2021-02-09 23:27:19 -0500

    feat: renders html for in product help xp

--- plugins/gatsby-plugin-generate-doc-json/gatsby-node.js
+++ plugins/gatsby-plugin-generate-doc-json/gatsby-node.js
@@ -1,12 +1,32 @@
 const fs = require('fs');
 const path = require('path');
 const unified = require('unified');
-const toHast = require('mdast-util-to-hast');
 const html = require('rehype-stringify');
 const removeImports = require('remark-mdx-remove-imports');
 const removeExports = require('remark-mdx-remove-exports');
+const fencedCodeBlock = require('../../codemods/fencedCodeBlock');
+const customHeadingIds = require('../gatsby-remark-custom-heading-ids/utils/visitor');
+const toHast = require('mdast-util-to-hast');
+const handlers = require('./utils/handlers');
+const all = require('mdast-util-to-hast/lib/all');
+const format = require('rehype-format');
 
-const htmlGenerator = unified().use(removeImports).use(removeExports).use(html);
+const mdxElement = (h, node) => {
+  const handler = handlers[node.name];
+
+  if (!handler) {
+    return all(h, node);
+  }
+  return handler(h, node);
+};
+
+const htmlGenerator = unified()
+  .use(removeImports)
+  .use(removeExports)
+  .use(fencedCodeBlock)
+  .use(customHeadingIds)
+  .use(html)
+  .use(format);
 
 exports.onPostBuild = async ({ graphql, store }) => {
   const { program } = store.getState();
@@ -23,18 +43,26 @@ exports.onPostBuild = async ({ graphql, store }) => {
   try {
     const { data } = await graphql(query);
 
-    data.allMdx.nodes.forEach((node) => {
+    data.allMdx.nodes.forEach(async (node) => {
       const { slug, mdxAST } = node;
 
       const filepath = path.join(program.directory, 'public', `${slug}.json`);
 
       const transformedAST = htmlGenerator.runSync(mdxAST);
-      const html = htmlGenerator.stringify(toHast(transformedAST));
+      const html = htmlGenerator.stringify(
+        toHast(transformedAST, {
+          handlers: {
+            mdxSpanElement: mdxElement,
+            mdxBlockElement: mdxElement,
+            code: handlers.CodeBlock,
+          },
+        })
+      );
       const result = { body: html };
 
       fs.writeFileSync(filepath, JSON.stringify(result));
     });
   } catch (error) {
-    console.error(`Unable to fetch data for JSON: ${error}`);
+    console.error(`Unable to fetch data for doc JSON: ${error}`);
   }
 };
---
+++ plugins/gatsby-plugin-generate-doc-json/utils/handlers.js
@@ -0,0 +1,133 @@
+const all = require('mdast-util-to-hast/lib/all');
+const { findAttribute } = require('../../../codemods/utils/mdxast');
+const toString = require('mdast-util-to-string');
+const u = require('unist-builder');
+
+const stripNulls = (obj) =>
+  Object.fromEntries(Object.entries(obj).filter(([, value]) => value != null));
+
+const getAllAttributes = (node) =>
+  node.attributes
+    ? node.attributes.reduce((obj, { name: propName, value }) => {
+        return { ...obj, [propName]: value };
+      }, {})
+    : {};
+
+module.exports = {
+  CodeBlock: (h, node) => {
+    const props =
+      node.meta &&
+      node.meta.split(' ').reduce((obj, metaProp) => {
+        const [propName, propValue] = metaProp.split('=');
+        return {
+          ...obj,
+          [propName]: propValue,
+        };
+      });
+
+    return h(
+      node,
+      'pre',
+      {
+        ...stripNulls({ ...props, lang: node.lang }),
+      },
+      [h(node, 'code', {}, [u('text', toString(node))])]
+    );
+  },
+  Callout: (h, node) => {
+    return h(
+      node,
+      'div',
+      {
+        className: `callout-${findAttribute('variant', node)}`,
+      },
+      [u('text', toString(node))]
+    );
+  },
+  Button: (h, node) => {
+    const className = [
+      findAttribute('variant', node),
+      findAttribute('size', node),
+    ].filter(Boolean);
+    return h(
+      node,
+      'button',
+      {
+        className: className.reduce(
+          (str, prop) => str.append(` btn-${prop}`),
+          'btn'
+        ),
+      },
+      [u('text', toString(node))]
+    );
+  },
+  ButtonLink: (h, node) => {
+    const className = [
+      findAttribute('variant', node),
+      findAttribute('size', node),
+    ].filter(Boolean);
+    return h(
+      node,
+      'a',
+      {
+        className: className.reduce((arr, prop) => [...arr, `btn-${prop}`], [
+          'btn',
+        ]),
+        href: findAttribute('to', node),
+      },
+      [u('text', toString(node))]
+    );
+  },
+  Collapser: (h, node) => {
+    return h(
+      node,
+      'div',
+      {
+        className: 'collapser',
+        title: findAttribute('title', node),
+      },
+      all(h, node)
+    );
+  },
+  CollapserGroup: (h, node) => {
+    return h(
+      node,
+      'div',
+      {
+        className: 'collapser-group',
+      },
+      all(h, node)
+    );
+  },
+  ExternalLink: (h, node) =>
+    h(node, 'a', stripNulls(getAllAttributes(node)), all(h, node)),
+  Link: (h, node) =>
+    h(
+      node,
+      'a',
+      {
+        href: `https://docs.newrelic.com${findAttribute('to', node)}`,
+      },
+      all(h, node)
+    ),
+  Icon: (h, node) => {
+    return h(
+      node,
+      'span',
+      {
+        className: 'icon',
+        name: findAttribute('name', node),
+      },
+      [u('text', '\u00A0')]
+    );
+  },
+  InlineCode: (h, node) => h(node, 'code', {}, [u('text', toString(node))]),
+  table: (h, node) => h(node, 'table', {}, all(h, node)),
+  thead: (h, node) => h(node, 'thead', {}, all(h, node)),
+  tbody: (h, node) => h(node, 'tbody', {}, all(h, node)),
+  tr: (h, node) => h(node, 'tr', {}, all(h, node)),
+  th: (h, node) => h(node, 'tr', {}, all(h, node)),
+  td: (h, node) => h(node, 'tr', {}, all(h, node)),
+  var: (h, node) => h(node, 'tr', {}, all(h, node)),
+  mark: (h, node) => h(node, 'tr', {}, all(h, node)),
+};
