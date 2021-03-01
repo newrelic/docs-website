6a2b526de1986364f9fe1b10ebf3d4b537f4198d
Cayla Hamann <chamann@newrelic.com>
2021-03-01 12:29:58 -0500

    chore: check for node value

--- plugins/gatsby-plugin-generate-doc-json/utils/handlers.js
+++ plugins/gatsby-plugin-generate-doc-json/utils/handlers.js
@@ -151,7 +151,7 @@ module.exports = {
         'div',
         {
           className: 'collapser',
-          title: titleNode.value.trim() || '',
+          title: titleNode.value?.trim() || '',
         },
         all(h, node)
       );
