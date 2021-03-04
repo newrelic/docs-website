577cb6b013c649210ff4148d60f7649f112aefc3
Zack Stickles <zstickles@newrelic.com>
2021-03-04 14:18:39 -0800

    chore: utility to get the agent name

---
+++ src/utils/getAgentName.js
@@ -0,0 +1,30 @@
+// this omits Synthetics private minions, Insights apps, tvOS agents, and unity plugins
+const AGENTS = [
+  '.net',
+  'android',
+  'browser',
+  'dotnet',
+  'go',
+  'infrastructure',
+  'ios',
+  'java',
+  'node',
+  'nodejs',
+  'php',
+  'python',
+  'ruby',
+  'sdk',
+];
+
+/**
+ * @param {string} subject The release note "subject" in frontmatter
+ * @returns {string}
+ */
+const getAgentName = (subject) =>
+  AGENTS.find(
+    (agent) =>
+      subject.toLowerCase().includes(agent) &&
+      !subject.toLowerCase().includes('insights')
+  );
+
+module.exports = getAgentName;
