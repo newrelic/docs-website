cb5821610122d13bbabba7f824acbd4569b3c294
Kris Kenney <70179215+nr-kkenney@users.noreply.github.com>
2021-03-29 16:20:50 -0700

    chore: update script. fix for removing old url with http protocol

--- scripts/utils/migrate/codemod-update-jp-url.js
+++ scripts/utils/migrate/codemod-update-jp-url.js
@@ -1,7 +1,7 @@
 const visit = require('unist-util-visit');
 const is = require('unist-util-is');
 
-const URL_TO_REMOVE = 'https://jajp-newrelic-docs.onelink-translations.com';
+const URL_TO_REMOVE = '//jajp-newrelic-docs.onelink-translations.com';
 
 const jpLinks = () => (tree) => {
   visit(
@@ -11,7 +11,7 @@ const jpLinks = () => (tree) => {
       if (node.url.includes(URL_TO_REMOVE)) {
         const url = new URL(node.url);
 
-        node.url = `https://docs.newrelic.co.jp${url.pathname}${url.search}${url.hash}`;
+        node.url = `${url.protocol}//docs.newrelic.co.jp${url.pathname}${url.search}${url.hash}`;
       }
     }
   );
