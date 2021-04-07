2c43adc0eb0ab8911cce8dc9c0f8bdfa95c598cb
John P Vajda <jvajda@newrelic.com>
2021-04-06 18:05:05 -0600

    chore: updated css

--- src/components/Watermark.js
+++ src/components/Watermark.js
@@ -7,15 +7,17 @@ const Watermark = ({ text }) => (
     data-swiftype-index={false}
     css={css`
       position: fixed;
+      left: 25%;
+      top: 75%;
+      transform: translate(-25%, -75%);
       color: #eeeeee;
       font-size: 18rem;
-      transform: rotate(-15deg);
+      transform: rotate(-25deg);
       z-index: -1;
       top: calc(var(--global-header-height) + 8rem);
       user-select: none;
       white-space: pre;
       line-height: 1;
-
       .dark-mode & {
         color: var(--color-dark-100);
       }
