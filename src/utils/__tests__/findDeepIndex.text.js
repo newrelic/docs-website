39ade136383a38ce064cf0e6e3c23951f40a5ce0
Zack Stickles <zstickles@gmail.com>
2021-04-19 16:32:02 -0700

    chore: allowing for differently names children

--- src/utils/__tests__/findDeepIndex.text.js
+++ src/utils/__tests__/findDeepIndex.text.js
@@ -17,36 +17,61 @@ const TREE = {
 };
 
 it('should find an index on the top-level', () => {
-  const fn = (path) => (child) => child.path === path;
+  const byPath = (path) => (child) => child.path === path;
 
-  expect(findDeepIndex(TREE, fn('/foo'))).toEqual(0);
-  expect(findDeepIndex(TREE, fn('/bar'))).toEqual(1);
-  expect(findDeepIndex(TREE, fn('/baz'))).toEqual(2);
+  expect(findDeepIndex(TREE, byPath('/foo'))).toEqual(0);
+  expect(findDeepIndex(TREE, byPath('/bar'))).toEqual(1);
+  expect(findDeepIndex(TREE, byPath('/baz'))).toEqual(2);
 });
 
 it('should find an index in a nested level', () => {
-  const fn = (path) => (child) => child.path === path;
+  const byPath = (path) => (child) => child.path === path;
 
-  expect(findDeepIndex(TREE, fn('/baz/apple'))).toEqual(0);
-  expect(findDeepIndex(TREE, fn('/baz/orange'))).toEqual(1);
+  expect(findDeepIndex(TREE, byPath('/baz/apple'))).toEqual(0);
+  expect(findDeepIndex(TREE, byPath('/baz/orange'))).toEqual(1);
 });
 
 it('should return `undefined` if a match not found', () => {
-  const fn = (path) => (child) => child.path === path;
+  const byPath = (path) => (child) => child.path === path;
 
-  expect(findDeepIndex(TREE, fn('/pumpkin'))).toEqual(undefined);
-  expect(findDeepIndex(TREE, fn('/baz/banana'))).toEqual(undefined);
+  expect(findDeepIndex(TREE, byPath('/pumpkin'))).toEqual(undefined);
+  expect(findDeepIndex(TREE, byPath('/baz/banana'))).toEqual(undefined);
 });
 
 it('should allow for different equality functions', () => {
-  const fn1 = (title) => (child) => child.title === title;
-  const fn2 = (length) => (child) => child.title.length === length;
+  const byTitle = (title) => (child) => child.title === title;
+  const byLength = (length) => (child) => child.title.length === length;
 
-  expect(findDeepIndex(TREE, fn1('Foo'))).toEqual(0);
-  expect(findDeepIndex(TREE, fn1('Bar'))).toEqual(1);
-  expect(findDeepIndex(TREE, fn1('Orange'))).toEqual(1);
+  expect(findDeepIndex(TREE, byTitle('Foo'))).toEqual(0);
+  expect(findDeepIndex(TREE, byTitle('Bar'))).toEqual(1);
+  expect(findDeepIndex(TREE, byTitle('Orange'))).toEqual(1);
 
-  expect(findDeepIndex(TREE, fn2(3))).toEqual(0);
-  expect(findDeepIndex(TREE, fn2(5))).toEqual(0);
-  expect(findDeepIndex(TREE, fn2(6))).toEqual(1);
+  expect(findDeepIndex(TREE, byLength(3))).toEqual(0);
+  expect(findDeepIndex(TREE, byLength(5))).toEqual(0);
+  expect(findDeepIndex(TREE, byLength(6))).toEqual(1);
+});
+
+it('should allow for different keys for "children"', () => {
+  const tree = {
+    name: 'Fruit',
+    types: [
+      { name: 'Banana', color: 'yellow' },
+      { name: 'Orange', color: 'orange' },
+      {
+        name: 'Berry',
+        types: [
+          { name: 'Strawberry', color: 'red' },
+          { name: 'Blueberry', color: 'blue' },
+        ],
+      },
+    ],
+  };
+
+  const byColor = (color) => (child) => child.color === color;
+
+  expect(findDeepIndex(tree, byColor('yellow'), 'types')).toEqual(0);
+  expect(findDeepIndex(tree, byColor('orange'), 'types')).toEqual(1);
+  expect(findDeepIndex(tree, byColor('red'), 'types')).toEqual(0);
+  expect(findDeepIndex(tree, byColor('blue'), 'types')).toEqual(1);
+  expect(findDeepIndex(tree, byColor('purple'), 'types')).toEqual(undefined);
 });
