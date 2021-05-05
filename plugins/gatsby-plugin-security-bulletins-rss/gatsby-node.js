b3cfccc74916b5d1b9950f61e5c7ad375bdc9f94
Clinton <clinton.langosch@gmail.com>
2021-05-05 14:37:57 -0700

    refactor: update pubdate. infer bulletins dir to generate feed path. simplify query. use meta description if avaialble

--- plugins/gatsby-plugin-security-bulletins-rss/gatsby-node.js
+++ plugins/gatsby-plugin-security-bulletins-rss/gatsby-node.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const RSS = require('rss');
 const format = require('date-fns/format');
-const parseISO = require('date-fns/parseISO');
 const html = require('rehype-stringify');
 const unified = require('unified');
 const toHast = require('mdast-util-to-hast');
@@ -25,19 +24,16 @@ const securityBulletinsQuery = async (graphql) => {
           siteUrl
         }
       }
-      securityBulletinFileNodes: allFile(
-        filter: {childMdx: {fileAbsolutePath: {regex: "/src/content/docs/security/new-relic-security/security-bulletins/.*(?<!index).mdx/"}}}
+      allMdx(
+        filter: {fileAbsolutePath: {regex: "/src/content/docs/security/.*/security-bulletins/.*(?<!index).mdx/"}}
       ) {
         nodes {
-          birthTime(formatString: "YYYY-MM-DD")
-          childMdx {
-            frontmatter {
-              title
-              metaDescription
-            }
-            slug
-            mdxAST
+          frontmatter {
+            title
+            metaDescription
           }
+          slug
+          mdxAST
         }
       }
     }
@@ -49,18 +45,17 @@ const securityBulletinsQuery = async (graphql) => {
 };
 
 const getFeedItem = (node, siteMetadata) => {
-  const { birthTime, childMdx } = node;
-  const { frontmatter, slug, mdxAST } = childMdx;
-  const { title } = frontmatter;
+  const { frontmatter, slug, mdxAST } = node;
+  const { title, metaDescription } = frontmatter;
 
   const transformedAST = htmlGenerator.runSync(mdxAST);
   const html = htmlGenerator.stringify(toHast(transformedAST));
 
   // time is necessary for RSS validity
-  const date = parseISO(birthTime);
+  const date = new Date();
   const pubDate = `${format(date, 'EE, dd LLL yyyy')} 00:00:00 +0000`;
   const link = new URL(slug, siteMetadata.siteUrl).href;
-  const id = Buffer.from(`${birthTime}-${title}`).toString('base64');
+  const id = Buffer.from(`${title}`).toString('base64');
 
   return {
     guid: id,
@@ -69,15 +64,18 @@ const getFeedItem = (node, siteMetadata) => {
       { link },
       { pubDate },
       { 'content:encoded': html },
-      { description: `CreatedOn: ${pubDate}.` },
+      { description: metaDescription || `Published: ${pubDate}.` },
     ],
   };
 };
 
 const generateFeed = (publicDir, siteMetadata, reporter, bulletinNodes) => {
   const title = `New Relic security bulletins`;
+  const inferredBulletinsPath = bulletinNodes.nodes[0].slug
+    .split('/')
+    .slice(0, -1);
 
-  const feedPath = path.join('security-bulletins', 'feed.xml');
+  const feedPath = path.join(inferredBulletinsPath, 'feed.xml');
 
   // https://github.com/dylang/node-rss#feedoptions
   const feedOptions = {
