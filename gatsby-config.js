e71615d26622c3ba5cc8656e4fe3587b54d88399
Shawn Kilburn <skilburn@newrelic.com>
2021-05-19 11:34:58 -0700

    Force a cache refresh so we can get that build check working

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
 const parse = require('rehype-parse');
+const fs = require('fs');
 const path = require('path');
 const unified = require('unified');
 const rehypeStringify = require('rehype-stringify');
