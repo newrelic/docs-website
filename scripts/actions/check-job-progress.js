d09d3da93def6621f85dc015b28042eb7f48e6ed
Zack Stickles <zstickles@gmail.com>
2021-05-28 17:32:12 -0700

    wip: better logging to troubleshoot the problem

--- scripts/actions/check-job-progress.js
+++ scripts/actions/check-job-progress.js
@@ -6,6 +6,7 @@ const fetchAndDeserialize = require('./fetch-and-deserialize');
 const PROJECT_ID = process.env.TRANSLATION_VENDOR_PROJECT;
 
 const uniq = (arr) => [...new Set(arr)];
+const prop = (key) => (x) => x[key];
 
 /**
  * @typedef {Object} Batch
@@ -20,28 +21,45 @@ const uniq = (arr) => [...new Set(arr)];
  * @returns {(batchUid: string) => Promise<Batch>}
  */
 const getBatchStatus = (accessToken) => async (batchUid) => {
-  const batchData = await vendorRequest({
-    method: 'GET',
-    endpoint: `/job-batches-api/v2/projects/${PROJECT_ID}/batches/${batchUid}`,
-    accessToken,
-  });
+  console.log(`[*] Getting status for batch: ${batchUid}`);
+  try {
+    const batchData = await vendorRequest({
+        method: 'GET',
+        endpoint: `/job-batches-api/v2/projects/${PROJECT_ID}/batches/${batchUid}`,
+        accessToken,
+    });
 
-  const { status, files, translationJobUid } = batchData;
+    const { status, files, translationJobUid } = batchData;
 
-  const jobData = await vendorRequest({
-    method: 'GET',
-    endpoint: `/jobs-api/v3/projects/${PROJECT_ID}/jobs/${translationJobUid}`,
-    accessToken,
-  });
+    const jobData = await vendorRequest({
+        method: 'GET',
+        endpoint: `/jobs-api/v3/projects/${PROJECT_ID}/jobs/${translationJobUid}`,
+        accessToken,
+    });
 
-  const { jobStatus } = jobData;
+    const { jobStatus } = jobData;
 
-  return {
-    batchUid,
-    done: jobStatus === 'COMPLETED',
-    locale: files[0].targetLocales[0].localeId,
-    fileUris: files.map((file) => file.fileUri),
-  };
+    return {
+        batchUid,
+        done: jobStatus === 'COMPLETED',
+        locale: files[0].targetLocales[0].localeId,
+        fileUris: files.map((file) => file.fileUri),
+    };
+  } catch(error) {
+    const { errors } = JSON.parse(error.message);
+
+    // if the batch cant be found, return null and process the rest
+    if (errors.map(prop('key')).includes('batch.not.found')) {
+      for (const { message } of errors) {
+        console.log(`[!] ${message}`);
+      }
+
+      return null;
+    }
+
+    // otherwise, something wrong happened, stop the script
+    throw error;
+  }
 };
 
 /** Entrypoint. */
@@ -74,6 +92,8 @@ const main = async () => {
     console.log(
       `::set-output name=batchesToDeserialize::${batchesToDeserialize.length}`
     );
+    process.exit(0);
+    return false;
 
     await Promise.all(
       batchesToDeserialize.map(fetchAndDeserialize(accessToken))
