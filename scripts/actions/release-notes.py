adbb92f52726aedf872e1ac15d0c5c2d03e7147d
rudouglas <ruairidouglas@gmail.com>
2021-06-01 18:27:11 +0100

    chore: refactor line formatting and variable names

--- scripts/actions/release-notes.py
+++ scripts/actions/release-notes.py
@@ -7,24 +7,24 @@ import re
 token = os.getenv('GITHUB_TOKEN', '...')
 
 # Initialize GitHub with token
-g = Github(token)
+github = Github(token)
 
 # Create heading for Release Notes
 result = "## :rocket: What's new?\n\n\n"
 
 # Get the Docs repo
-repo = g.get_repo("newrelic/docs-website")
+repo = github.get_repo("newrelic/docs-website")
 
 # Compare diff between main and develop
 diff = repo.compare("main", "develop")
 
 # Loop through commits and add details to result
-for com in diff.commits:
-  line = "@" + com.author.login
-  line += " - " + com.commit.message.splitlines()[0]
-  line += " [->](" + com.html_url + ")"
-  result += line
-  result += "\n"
+for commit in diff.commits:
+  result += "@{author} - {message} [->]({url})\n".format(
+    author=commit.author.login,
+    message=commit.commit.message.splitlines()[0],
+    url=commit.html_url
+  )
 
 # Set result as an Env for use in Workflow
 run('echo "RESULT<<EOF" >> $GITHUB_ENV')
