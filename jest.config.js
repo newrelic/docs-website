ee52b3fb03e1960b6ca1f36a97a332acb2402217
Zack Stickles <zstickles@gmail.com>
2021-06-14 11:13:29 -0700

    chore: ignoring all mock dirs

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,12 @@ module.exports = {
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/file-mock.js',
   },
-  testPathIgnorePatterns: ['node_modules', '\\.cache', '<rootDir>.*/public'],
+  testPathIgnorePatterns: [
+    'node_modules',
+    '\\.cache',
+    '<rootDir>.*/public',
+    '__mocks__',
+  ],
   transformIgnorePatterns: [
     'node_modules/(?!(gatsby)||@newrelic/gatsby-theme-newrelic||@elastic/react-search-ui-views/)',
   ],
