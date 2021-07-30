a65960679fb4a02b0ab171c466255b5494f89f4d
Elaine Laguerta <elaguerta@newrelic.com>
2021-07-30 15:57:18 -0700

    chore: change script so that the user can run it from anywhere

--- scripts/actions/translation_workflow/testing/script.sh
+++ scripts/actions/translation_workflow/testing/script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+pushd $SCRIPT_DIR/../../../..
+
 # # step 1
-export DB_CONNECTION_INFO=$(cat connection_info.json)
+export DB_CONNECTION_INFO=$(cat $SCRIPT_DIR/connection_info.json)
 export GITHUB_TOKEN='' # token with no permissions
 
 # # step 1
@@ -14,13 +18,16 @@ export TRANSLATION_VENDOR_PROJECT='' # use machine translation project
 export TRANSLATION_VENDOR_USER='' # MT user
 export TRANSLATION_VENDOR_SECRET='' # MT user secret
 
-node ../../send-and-update-translation-queue.js
+
+node $SCRIPT_DIR/../../send-and-update-translation-queue.js
 
 # wait for translation to be complete
-node ./wait_for_translation.js
+node $SCRIPT_DIR/./wait_for_translation.js
 
 # # step 3
-node ../../check-job-progress.js
+node $SCRIPT_DIR/../../check-job-progress.js
 
 # # step 4
-node ../../remove-completed-batch.js
+node $SCRIPT_DIR/../../remove-completed-batch.js
+
+popd
