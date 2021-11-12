ae4b2498e2119e26e24b8809f4dff2ad6f7b60d6
lbaker <lbaker@newrelic.com>
2021-11-11 16:22:49 -0800

    chore: Add megaphone icon

--- src/@newrelic/gatsby-theme-newrelic/icons/newrelic.js
+++ src/@newrelic/gatsby-theme-newrelic/icons/newrelic.js
@@ -1,7 +1,9 @@
 import defaultIcons from '@newrelic/gatsby-theme-newrelic/src/icons/newrelic';
 import CaretUp from './newrelic/caret-up';
+import InfoAnnouncement from './newrelic/info-announcement';
 
 export default {
   ...defaultIcons,
   'caret-up': CaretUp,
+  'info-announcement': InfoAnnouncement,
 };
