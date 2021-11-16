7e17ad749b270e4f6808bb6a00c3adffa203a85c
lbaker <lbaker@newrelic.com>
2021-11-16 11:13:24 -0800

    chore: Add icons

--- src/@newrelic/gatsby-theme-newrelic/icons/newrelic.js
+++ src/@newrelic/gatsby-theme-newrelic/icons/newrelic.js
@@ -1,9 +1,11 @@
 import defaultIcons from '@newrelic/gatsby-theme-newrelic/src/icons/newrelic';
 import CaretUp from './newrelic/caret-up';
 import InfoAnnouncement from './newrelic/info-announcement';
+import Dashboard from './newrelic/dashboard';
 
 export default {
   ...defaultIcons,
   'caret-up': CaretUp,
   'info-announcement': InfoAnnouncement,
+  dashboard: Dashboard,
 };
