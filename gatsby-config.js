a22e90598c901238a56fef6a2db14d20b47f5f38
rhetoric101 <robert@robertsiebens.com>
2022-02-14 17:13:52 -0800

    fix(Network monitoring): Test out shell session Prism option for Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,7 @@ module.exports = {
             'php',
             'phpdoc',
             'csharp',
-            'shell',
+            'shell-session',
             'python',
           ],
         },
