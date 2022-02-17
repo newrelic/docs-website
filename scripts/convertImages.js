361bf8fc7cab902d85d0aea8c5d3bbd449742239
Clinton <clinton.langosch@gmail.com>
2022-02-17 11:51:49 -0800

    wip: update existing import statement paths to new aliased path

--- scripts/convertImages.js
+++ scripts/convertImages.js
@@ -7,12 +7,8 @@ const remarkMdxjs = require('remark-mdxjs');
 const visit = require('unist-util-visit');
 const u = require('unist-builder');
 const camelCase = require('camelcase');
-// const compile = require('@mdx-js/mdx');
-// const rehypeParse = require('rehype-parse');
-// const remark2rehype = require('remark-rehype');
 const frontmatter = require('remark-frontmatter');
 const remarkStringify = require('remark-stringify');
-const { log } = require('console');
 
 const filePath = path.join(
   process.cwd(),
@@ -26,47 +22,53 @@ const images = () => {
   // return transformer;
   return (tree, file) => {
     const imports = [];
-    visit(tree, 'image', (node, index, parent) => {
-      // Maybe reference https://github.com/remcohaszing/remark-mdx-images
-      // for hints on what we need to do
+    visit(
+      tree,
+      (node) => ['image', 'import'].includes(node.type),
+      (node, index, parent) => {
+        if (node.type === 'import') {
+          node.value = node.value.replace('./images/', 'images/');
+        }
+        if (node.type === 'image') {
+          let { alt = null, title, url } = node;
 
-      let { alt = null, title, url } = node;
+          if (absoluteUrlPattern.test(url)) {
+            return;
+          }
 
-      if (absoluteUrlPattern.test(url)) {
-        return;
-      }
-
-      if (relativePathPattern.test(url)) {
-        const importName = camelCase(
-          node.url.replace('./images/', '').replace(/\.(png|jpg)/i, '')
-        );
-        const importString = `import ${importName} from '${node.url.replace(
-          './',
-          ''
-        )}'`;
-        imports.push(importString);
-        node = u(
-          'mdxBlockElement',
-          {
-            name: 'img',
-            attributes: [
+          if (relativePathPattern.test(url)) {
+            const importName = camelCase(
+              node.url.replace('./images/', '').replace(/\.(png|jpg)/i, '')
+            );
+            const importString = `import ${importName} from '${node.url.replace(
+              './',
+              ''
+            )}'`;
+            imports.push(importString);
+            node = u(
+              'mdxBlockElement',
               {
-                type: 'mdxAttribute',
-                name: 'src',
-                value: {
-                  type: 'mdxValueExpression',
-                  value: importName,
-                },
+                name: 'img',
+                attributes: [
+                  {
+                    type: 'mdxAttribute',
+                    name: 'src',
+                    value: {
+                      type: 'mdxValueExpression',
+                      value: importName,
+                    },
+                  },
+                ],
+                position: node.position,
               },
-            ],
-            position: node.position,
-          },
-          []
-        );
+              []
+            );
 
-        parent.children.splice(index, 1, node);
+            parent.children.splice(index, 1, node);
+          }
+        }
       }
-    });
+    );
     visit(tree, 'root', (node) => {
       const [head, ...tail] = node.children;
       const importNode = u('paragraph', {
@@ -82,6 +84,7 @@ const mdxFile = fs.readFileSync(path.join(filePath));
 
 // const mdxAst = unified()
 //   .use(remarkParse)
+//   .use(remarkMdxjs)
 //   .use(frontmatter, ['yaml'])
 //   .parse(mdxFile);
 
@@ -102,6 +105,7 @@ const processor = unified()
   .use(remarkMdxjs)
   .use(frontmatter, ['yaml'])
   .use(images);
+
 const main = async () => {
   const { contents } = await processor.process(mdxFile);
   fs.writeFileSync(path.join(process.cwd(), 'newMdx.mdx'), contents, 'utf-8');
