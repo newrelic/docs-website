a2e07c22c3ac8c01d10b83202f002b2fa60a0d2d
Clark McAdoo <jmcadoo@newrelic.com>
2022-04-06 15:54:25 -0500

    WIP: add test PR to see if gatsby will build on-pre-render-html scripts

---
+++ gatsby/on-pre-render-html.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const onPreRenderHtml = ({
+  getPostBodyComponents,
+  replacePostBodyComponents,
+}) => {
+  console.log('test');
+
+  replacePostBodyComponents([
+    ...getPostBodyComponents,
+    <script>
+      !function(e,t,n,s,u,a)
+      {e.twq ||
+        ((s = e.twq = function () {
+          s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
+        }),
+        (s.version = '1.1'),
+        (s.queue = []),
+        (u = t.createElement(n)),
+        (u.async = !0),
+        (u.src = '//static.ads-twitter.com/uwt.js'),
+        (a = t.getElementsByTagName(n)[0]),
+        a.parentNode.insertBefore(u, a))}
+      (window,document,'script'); // Insert Twitter Pixel ID and Standard Event
+      data below twq('init','o73vi'); twq('track','PageView');
+    </script>,
+  ]);
+};
+
+export default onPreRenderHtml;
