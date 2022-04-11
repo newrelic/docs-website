ee4ad15bdc3a8fb33c8129eedb01bae8c03a3020
Clark McAdoo <jmcadoo@newrelic.com>
2022-04-11 13:53:18 -0500

    fix: add on-render-body file/function to be used in the onRenderBody Gatsby API function

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,31 +3,6 @@
  *
  * See: https://www.gatsbyjs.org/   docs/ssr-apis/
  */
-const React = require("react")
-
-const addTwitterTag = [
-    <script>
-        !function(e,t,n,s,u,a)
-        {e.twq ||
-        ((s = e.twq = function () {
-            s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
-        }),
-            (s.version = '1.1'),
-            (s.queue = []),
-            (u = t.createElement(n)),
-            (u.async = !0),
-            (u.src = '//static.ads-twitter.com/uwt.js'),
-            (a = t.getElementsByTagName(n)[0]),
-            a.parentNode.insertBefore(u, a))}
-        (window,document,'script'); // Insert Twitter Pixel ID and Standard Event
-        data below twq('init','o73vi'); twq('track','PageView');
-    </script>
-]
-  
-export default onRenderBody = ({
-    setPostBodyComponents
-}) => {
-    setPostBodyComponents(addTwitterTag)
-    }
 
 export { default as wrapPageElement } from './gatsby/wrap-page-element';
+export { default as onRenderBody } from './gatsby/on-render-body';
---
+++ gatsby/on-render-body.js
@@ -0,0 +1,26 @@
+const React = require('react');
+
+const onRenderBody = ({ setPostBodyComponents }) => {
+  const addTwitterTag = (
+    <script>
+      !function(e,t,n,s,u,a)
+      {e.twq ||
+        ((s = e.twq = function () {
+          s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
+        }),
+        (s.version = '1.1'),
+        (s.queue = []),
+        (u = t.createElement(n)),
+        (u.async = !0),
+        (u.src = '//static.ads-twitter.com/uwt.js'),
+        (a = t.getElementsByTagName(n)[0]),
+        a.parentNode.insertBefore(u, a))}
+      (window,document,'script'); // Insert Twitter Pixel ID and Standard Event
+      data below twq('init','o73vi'); twq('track','PageView');
+    </script>
+  );
+
+  setPostBodyComponents([addTwitterTag]);
+};
+
+export default onRenderBody;
