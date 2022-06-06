fb665dca2ec2f43c1dd81e4f312582980eab78c1
lbaker <lbaker@newrelic.com>
2022-06-06 10:29:40 -0700

    chore: Add translation nav script

---
+++ scripts/getNavForTranslation.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+
+const isFile = (filepath) => fs.statSync(filepath).isFile();
+const pathJoin = (base) => (filepath) => path.join(base, filepath);
+const skippedTitles = ['Style guide', 'root'];
+const locale = process.argv.slice(2)[0];
+
+const printExample = () => console.warn('Example: yarn generate-nav-titles jp');
+console.log(locale);
+if (!locale) {
+  console.warn('<!> No locale specified, please append to the command');
+  printExample();
+  process.exit(0);
+}
+
+const getNavFiles = () => {
+  const rawFiles = fs
+    .readdirSync('src/nav')
+    .map(pathJoin('src/nav'))
+    .filter(isFile);
+
+  const parsedFiles = rawFiles.map((file) => {
+    const fileContents = fs.readFileSync(file, 'utf8');
+    const data = yaml.safeLoad(fileContents);
+    return data;
+  });
+  return parsedFiles;
+};
+
+const readSubNavs = (subNav, flatNav) => {
+  return subNav.flatMap((file) => {
+    const appendedNav = [
+      ...flatNav,
+      { englishTitle: file.title, title: file.title, locale },
+    ];
+    if (file.pages) {
+      return readSubNavs(file.pages, appendedNav);
+    } else {
+      return appendedNav;
+    }
+  });
+};
+
+const getFlatNav = () => {
+  const parsedFiles = getNavFiles();
+  const flatNav = parsedFiles.flatMap((file) => {
+    const navItem = { englishTitle: file.title, title: file.title, locale };
+    const appendedNav = [];
+    if (!skippedTitles.includes(file.title)) {
+      appendedNav.push(navItem);
+    }
+    return readSubNavs(file.pages, appendedNav);
+  });
+  return flatNav;
+};
+
+const filterUniqueTitles = () => {
+  const uniqueTitles = [];
+  const flatNav = getFlatNav(locale);
+  const titleList = flatNav.filter((navItem) => {
+    if (uniqueTitles.includes(navItem.title)) {
+      return false;
+    } else {
+      uniqueTitles.push(navItem.title);
+      return true;
+    }
+  });
+  fs.writeFileSync(`${locale}.json`, JSON.stringify(titleList));
+};
+
+filterUniqueTitles();
