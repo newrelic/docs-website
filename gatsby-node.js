8e82ff936a2616bcb5e36b9fab1875ffe2f6165b
Clark McAdoo <jmcadoo@newrelic.com>
2022-08-01 18:43:30 -0500

    fix: remove DSG temporarily

--- gatsby-node.js
+++ gatsby-node.js
@@ -253,7 +253,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
           createPage,
           disableSwiftype: !i18nNode,
         },
-        true // always defer localized pages
+        false // temporary disable DSG
       );
     });
   });
