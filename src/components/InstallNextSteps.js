ee164992163da6796768df706bb9d2df161e2640
Cayla Hamann <chamann@newrelic.com>
2022-09-20 11:18:51 -0400

    chore: Add install feedback as part of the whats next component

--- src/components/InstallNextSteps.js
+++ src/components/InstallNextSteps.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import InstallFeedback from './InstallFeedback';
 
 import MDXContainer from './MDXContainer';
 
 const InstallNextSteps = ({ mdx, className }) => {
   return (
     <div className={className}>
-      <InstallFeedback />
       {mdx?.body && <MDXContainer body={mdx?.body} />}
     </div>
   );
