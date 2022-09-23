7d0b31cae3ea6b1dc2c0430610855513ef80aeea
Liz Baker <lbaker@newrelic.com>
2022-09-22 22:12:03 -0700

    chore: Re-enable DSG

--- gatsby-node.js
+++ gatsby-node.js
@@ -253,7 +253,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
           createPage,
           disableSwiftype: !i18nNode,
         },
-        false // disable DSG
+        true // enable DSG
       );
     });
   });
