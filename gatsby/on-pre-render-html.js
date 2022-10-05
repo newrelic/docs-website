5d3701e7b3a1ba75bcf5a3ef4491814b459d5e7b
Cayla Hamann <chamann@newrelic.com>
2022-10-05 17:04:30 -0400

    fix: filter head components correctly on constants

--- gatsby/on-pre-render-html.js
+++ gatsby/on-pre-render-html.js
@@ -1,3 +1,5 @@
+import { FILTERED_HEAD_KEYS } from '../src/utils/constants';
+
 const onPreRenderHTML = ({
   getHeadComponents,
   replaceHeadComponents,
@@ -6,7 +8,9 @@ const onPreRenderHTML = ({
   const headComponents = getHeadComponents();
   if (pathname.endsWith('/embed') || pathname.endsWith('/embed/')) {
     return replaceHeadComponents([
-      headComponents.filter((component) => component.key === 'one-trust'),
+      headComponents.filter(
+        (component) => !FILTERED_HEAD_KEYS.includes(component.key)
+      ),
     ]);
   }
   return replaceHeadComponents([headComponents]);
