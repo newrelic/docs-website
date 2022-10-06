7a79e22324fe3d983fd504937c4762f7073fc086
Clark McAdoo <jmcadoo@newrelic.com>
2022-10-06 18:24:02 -0500

    WIP - add nav footer file

---
+++ src/components/NavFooter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { css } from '@emotion/react';
+
+const NavFooter = () => {
+  return (
+    <div
+      css={css`
+        background: #afe2e3;
+        height: 72px;
+        width: 100%;
+      `}
+    >
+      This is a nav footer
+    </div>
+  );
+};
+
+export default NavFooter;
