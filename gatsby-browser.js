dc51834e82e10f51f72091a28dbd01cc7adae697
Liz Baker <lbaker@newrelic.com>
2022-11-22 15:15:42 -0800

    test: Try setting public path

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,9 @@
 export { default as onRouteUpdate } from './gatsby/on-route-update';
 export { default as wrapPageElement } from './gatsby/wrap-page-element';
 export { default as shouldUpdateScroll } from './gatsby/should-update-scroll';
+const onClientEntry = () => {
+  // Expose both globals so that the NR1 docs can read it.
+  window.__webpack_public_path__ = 'https://docs-dev.newrelic.com/';
+};
+
+export { onClientEntry };
