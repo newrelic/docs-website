6b7d0ed6e89e38f4dc9d546bbabe5586aa676d5f
Clark McAdoo <jmcadoo@newrelic.com>
2023-02-09 14:57:08 -0700

    wip - get count of number of translation types in frontmatter

--- scripts/reformatFrontmatter.js
+++ scripts/reformatFrontmatter.js
@@ -1,28 +1,36 @@
 // const mdx = require('@mdx-js/mdx');
 const fs = require('fs');
 const glob = require('glob');
-const path = require('path');
 const frontmatter = require('@github-docs/frontmatter');
 
+let noTranslation = 0;
+let jpTranslation = 0;
+let krTranslation = 0;
+
 const updateFrontmatter = async (filePath) => {
-  try {
-    console.log(`reading ${filePath}`);
-    const contents = fs.readFileSync(filePath);
-    const { data } = frontmatter(contents);
-    console.log(data);
-  } catch (exception) {
-    console.log(JSON.stringify(exception, null, 4));
-    return filePath;
+  const contents = fs.readFileSync(filePath);
+  const { data } = frontmatter(contents);
+  if (data.translate && Array.isArray(data.translate)) {
+    if (data.translate.includes('jp')) {
+      jpTranslation++;
+    }
+    if (data.translate.includes('kr')) {
+      krTranslation++;
+    }
+  } else {
+    noTranslation++;
+    return;
   }
 };
 
 const main = async () => {
-  // const filePaths = glob.sync(`${__dirname}/../src/content/**/*.mdx`);
-  const testFile = path.join(process.cwd(), 'scripts/test.mdx');
-  updateFrontmatter(testFile);
-
-  // const allResults = await Promise.all(filePaths.map(readFile));
+  const filePaths = glob.sync(`${__dirname}/../src/content/**/*.mdx`);
+  await Promise.all(filePaths.map(updateFrontmatter));
   // const results = allResults.filter(Boolean);
+
+  console.log('noTranslation', noTranslation);
+  console.log('jpTranslation', jpTranslation);
+  console.log('krTranslation', krTranslation);
 };
 
 main();
