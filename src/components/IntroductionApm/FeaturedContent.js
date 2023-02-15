e8cfe4faaafbf65d41541d6a90a525ff5d4a28b1
Clark McAdoo <jmcadoo@newrelic.com>
2023-02-15 16:09:39 -0700

    wip - get images working, start work on list

--- src/components/IntroductionApm/FeaturedContent.js
+++ src/components/IntroductionApm/FeaturedContent.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import styled from '@emotion/styled';
-// import { Image } from '@newrelic/gatsby-theme-newrelic';
 
-export const FeaturedContent = ({ title, subtitle, text, img }) => {
+export const FeaturedContent = ({ img, list, subTitle, text, title }) => {
   return (
     <Container>
       <TextContainer>
-        {title && <h1>{title}</h1>}
-        {text && text.map((paragraph) => <p>{paragraph}</p>)}
+        {title && <Title>{title}</Title>}
+        {subTitle && <SubTitle>{subTitle}</SubTitle>}
+        {text && text.map((paragraph, i) => <p key={i}>{paragraph}</p>)}
+        <ListWrapper>
+          {list && list.map((item, i) => <li key={i}>{item}</li>)}
+        </ListWrapper>
       </TextContainer>
+      <ImageWrapper>
+        <img src={img} />
+      </ImageWrapper>
     </Container>
   );
 };
@@ -18,6 +24,33 @@ const Container = styled.div`
   width: 100%;
 `;
 
+const ListWrapper = styled.ul`
+  list-style-type: '→';
+`;
+
+const ImageWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  padding-left: 34px;
+  width: 34%;
+
+  img {
+    object-fit: contain;
+    width: 100%;
+  }
+`;
+
+const SubTitle = styled.h2`
+  font-size: 28px;
+`;
+
 const TextContainer = styled.div`
-  width: 100%;
+  border-right: 1px solid #f1b9df;
+  padding: 36px 32px 36px 0;
+  width: 66%;
+`;
+
+const Title = styled.h1`
+  font-size: 44px;
 `;
--- src/pages/introduction-apm.js
+++ src/pages/introduction-apm.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FeaturedContent } from '../components/IntroductionApm';
 
+import browserAjaxDetails from 'images/browser_screenshot-full_ajax-details.png';
+
 const introTextInput = [
   'Our application performance monitoring (APM) provides a unified monitoring service for all your apps and microservices.',
   "Monitor everything from the hundreds of dependencies of a modern stack down to simple web-transaction times and throughput of an app. Keep track of your app's health in real-time by monitoring your metrics, events, logs, and transactions (MELT) through pre-built and custom dashboards",
@@ -9,7 +11,19 @@ const introTextInput = [
 const introTitle = 'Complete visibility and analytics at your fingertips';
 
 const IntroductionApm = () => (
-  <FeaturedContent text={introTextInput} title={introTitle} />
+  <>
+    <FeaturedContent
+      text={introTextInput}
+      title={introTitle}
+      img={browserAjaxDetails}
+    />
+
+    <FeaturedContent
+      list={introTextInput}
+      subTitle={introTitle}
+      img={browserAjaxDetails}
+    />
+  </>
 );
 
 export default IntroductionApm;
