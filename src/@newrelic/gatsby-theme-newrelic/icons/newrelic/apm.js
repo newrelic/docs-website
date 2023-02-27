33bfea778716c86fc6d87864a735a5ff77c791ea
Tabatha Zeitke <tzeitke@newrelic.com>
2023-02-27 14:28:59 -0800

    fix: remove clipping from icon

--- src/@newrelic/gatsby-theme-newrelic/icons/newrelic/apm.js
+++ src/@newrelic/gatsby-theme-newrelic/icons/newrelic/apm.js
@@ -6,7 +6,7 @@ import SVG from '@newrelic/gatsby-theme-newrelic/src/components/SVG';
 const ApmIcon = (props) => (
   <SVG
     {...props}
-    viewBox="0 0 16 16"
+    viewBox="-.5 -.5 16 16"
     css={css`
       fill: currentColor;
       stroke: currentColor;
