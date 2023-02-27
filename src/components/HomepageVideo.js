e6f8e5b55e96bb2b07c6e6955571abe4839b66c6
Liz Baker <lbaker@newrelic.com>
2023-02-27 14:58:14 -0800

    chore: Refactor away from styled components

--- src/components/HomepageVideo.js
+++ src/components/HomepageVideo.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 import { SearchInput, useTranslation } from '@newrelic/gatsby-theme-newrelic';
 import { navigate } from '@reach/router';
@@ -22,54 +21,51 @@ const HomepageVideo = () => {
     }
   }, [sidebar]);
 
-  const PageContainer = styled.div`
-    padding-top: 7rem;
-    padding-bottom: 1rem;
-    width: 100%;
-    display: grid;
-    grid-template-areas: 'welcome video' 'break signup';
-    grid-template-columns: 2fr 3fr;
-    grid-gap: 1rem;
-    background-image: url(${curlyAndDotsBackground});
-    background-size: 70%;
-    background-repeat: no-repeat;
-    background-position: top -3vh right 20%;
-    .dark-mode & {
-      background-image: url(${curlyAndDotsBackgroundDarkmode});
-    }
-    @media screen and (max-width: ${mobileBreakpoint}) {
-      background-image: none;
-      display: flex;
-      flex-direction: column;
-      padding-top: 2rem;
-      .dark-mode & {
-        background-image: none;
-      }
-    }
-  `;
-  const WelcomeContainer = styled.div`
-    grid-area: welcome;
-    padding-right: 3rem;
-    padding-top: 0.5rem;
-    h1 {
-      font-size: 56px;
-      font-weight: 500;
-    }
-    @media screen and (max-width: ${mobileBreakpoint}) {
-      width: 100%;
-      margin-bottom: 2rem;
-    }
-  `;
-  const VideoContainer = styled.div`
-    grid-area: video;
-    @media screen and (max-width: ${mobileBreakpoint}) {
-      width: 100%;
-    }
-  `;
   return (
     <>
-      <PageContainer>
-        <WelcomeContainer>
+      <div
+        css={css`
+          padding-top: 7rem;
+          padding-bottom: 1rem;
+          width: 100%;
+          display: grid;
+          grid-template-areas: 'welcome video' 'break signup';
+          grid-template-columns: 2fr 3fr;
+          grid-gap: 1rem;
+          background-image: url(${curlyAndDotsBackground});
+          background-size: 70%;
+          background-repeat: no-repeat;
+          background-position: top -3vh right 20%;
+          .dark-mode & {
+            background-image: url(${curlyAndDotsBackgroundDarkmode});
+          }
+          @media screen and (max-width: ${mobileBreakpoint}) {
+            background-image: none;
+            display: flex;
+            flex-direction: column;
+            padding-top: 2rem;
+            .dark-mode & {
+              background-image: none;
+            }
+          }
+        `}
+      >
+        <div
+          css={css`
+            grid-area: welcome;
+            padding-right: 3rem;
+            padding-top: 0.5rem;
+            h1 {
+              font-size: 56px;
+              font-weight: 500;
+            }
+            @media screen and (max-width: ${mobileBreakpoint}) {
+              width: 100%;
+              padding-right: 0;
+              margin-bottom: 2rem;
+            }
+          `}
+        >
           <h1>{t('homepageVideo.header')}</h1>
           <p>{t('homepageVideo.p1')}</p>
           <p>{t('homepageVideo.p2')}</p>
@@ -80,7 +76,9 @@ const HomepageVideo = () => {
             iconName={SearchInput.ICONS.SEARCH}
             isIconClickable
             alignIcon={SearchInput.ICON_ALIGNMENT.RIGHT}
-            onChange={(e) => setSearchTerm(e.target.value)}
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
             onSubmit={() => navigate(`?q=${searchTerm || ''}`)}
             css={css`
               @media screen and (max-width: ${mobileBreakpoint}) {
@@ -88,8 +86,15 @@ const HomepageVideo = () => {
               }
             `}
           />
-        </WelcomeContainer>
-        <VideoContainer>
+        </div>
+        <div
+          css={css`
+            grid-area: video;
+            @media screen and (max-width: ${mobileBreakpoint}) {
+              width: 100%;
+            }
+          `}
+        >
           <div
             className="wistia_responsive_padding"
             style={{ padding: '56.25% 0 0 ', position: 'relative' }}
@@ -147,8 +152,8 @@ const HomepageVideo = () => {
               }
             `}
           />
-        </VideoContainer>
-      </PageContainer>
+        </div>
+      </div>
     </>
   );
 };
