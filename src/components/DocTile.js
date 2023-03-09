7fd1de0ec37c74a0dc992a467ef1d380ac56b319
Tabatha Zeitke <tzeitke@newrelic.com>
2023-03-09 12:20:12 -0800

    chore: add instrumentation category to DocTile

--- src/components/DocTile.js
+++ src/components/DocTile.js
@@ -4,115 +4,109 @@ import SurfaceLink from './SurfaceLink';
 import { css } from '@emotion/react';
 import PropTypes from 'prop-types';
 
-export const DocTile = ({
-  children,
-  path,
-  instrumentation,
-  label,
-  date,
-  number,
-  className,
-}) => (
-  <SurfaceLink
-    base={Surface.BASE.SECONDARY}
-    to={path}
-    interactive
-    instrumentation={instrumentation}
-    css={css`
-      display: block;
-      min-height: 130px;
-      border-radius: 4px;
-      background: var(--secondary-background-color);
-
-      .dark-mode & {
-        background: var(--secondary-background-color);
-      }
-    `}
-    className={className}
-  >
-    <div
+export const DocTile = ({ children, path, label, date, number, className }) => {
+  return (
+    <SurfaceLink
+      base={Surface.BASE.SECONDARY}
+      to={path}
+      interactive
+      instrumentation={{ component: 'docTile' }}
       css={css`
-        display: flex;
-        flex-direction: column;
-        height: 100%;
-        justify-content: space-between;
-        align-items: space-between;
-        padding: 2rem;
+        display: block;
+        min-height: 130px;
+        border-radius: 4px;
+        background: var(--secondary-background-color);
 
-        @media screen and (max-width: 650px) {
-          padding: 1.5rem;
+        .dark-mode & {
+          background: var(--secondary-background-color);
         }
       `}
+      className={className}
     >
-      <h4
-        css={css`
-          font-weight: 400;
-          font-size: 20px;
-          display: flex;
-          gap: 1rem;
-          margin-bottom: 1rem;
-        `}
-      >
-        {number && (
-          <span
-            css={css`
-              flex: none;
-              display: inline-flex;
-              justify-content: center;
-              align-items: center;
-              height: 2rem;
-              width: 2rem;
-              border-radius: 50%;
-              background: var(--brand-button-primary-accent);
-              color: var(--system-text-primary-light);
-            `}
-          >
-            {number}
-          </span>
-        )}
-        {children}
-      </h4>
       <div
         css={css`
           display: flex;
+          flex-direction: column;
+          height: 100%;
           justify-content: space-between;
+          align-items: space-between;
+          padding: 2rem;
+
+          @media screen and (max-width: 650px) {
+            padding: 1.5rem;
+          }
         `}
       >
-        {label && (
-          <Tag
-            css={css`
-              background: ${label.color};
-              color: var(--system-text-primary-light);
-
-              .dark-mode & {
+        <h4
+          css={css`
+            font-weight: 400;
+            font-size: 20px;
+            display: flex;
+            gap: 1rem;
+            margin-bottom: 1rem;
+          `}
+        >
+          {number && (
+            <span
+              css={css`
+                flex: none;
+                display: inline-flex;
+                justify-content: center;
+                align-items: center;
+                height: 2rem;
+                width: 2rem;
+                border-radius: 50%;
+                background: var(--brand-button-primary-accent);
+                color: var(--system-text-primary-light);
+              `}
+            >
+              {number}
+            </span>
+          )}
+          {children}
+        </h4>
+        <div
+          css={css`
+            display: flex;
+            justify-content: space-between;
+          `}
+        >
+          {label && (
+            <Tag
+              css={css`
                 background: ${label.color};
                 color: var(--system-text-primary-light);
-              }
-            `}
-          >
-            {label.text}
-          </Tag>
-        )}
-        {date && (
-          <Tag
+
+                .dark-mode & {
+                  background: ${label.color};
+                  color: var(--system-text-primary-light);
+                }
+              `}
+            >
+              {label.text}
+            </Tag>
+          )}
+          {date && (
+            <Tag
+              css={css`
+                color: var(--primary-text-color);
+              `}
+            >
+              {date}
+            </Tag>
+          )}
+          <Icon
+            name="fe-arrow-right"
             css={css`
               color: var(--primary-text-color);
+              margin-left: auto;
             `}
-          >
-            {date}
-          </Tag>
-        )}
-        <Icon
-          name="fe-arrow-right"
-          css={css`
-            color: var(--primary-text-color);
-            margin-left: auto;
-          `}
-        />
+          />
+        </div>
       </div>
-    </div>
-  </SurfaceLink>
-);
+    </SurfaceLink>
+  );
+};
 
 DocTile.propTypes = {
   label: PropTypes.array,
