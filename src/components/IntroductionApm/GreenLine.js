d13989964d3099880cd50eae5a6369cc5c842cbc
Clark McAdoo <jmcadoo@newrelic.com>
2023-03-17 14:44:18 -0500

    get green line to be animated

--- src/components/IntroductionApm/GreenLine.js
+++ src/components/IntroductionApm/GreenLine.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from '@emotion/styled';
 
-export const GreenLine = () => (
-  <Wrapper>
-    <Line />
-  </Wrapper>
-);
+export const GreenLine = () => {
+  let [calcHeight, setCalcHeight] = useState(0);
+  const lineRef = useRef(null);
+
+  const handleLineHeight = (h) => {
+    if (lineRef.current.getAttribute('height') >= h) return;
+    setCalcHeight(h);
+  };
+
+  const handleScroll = () => {
+    console.log(lineRef.current.getBoundingClientRect());
+    const totalHeight = lineRef.current.getBoundingClientRect().height;
+    let top = lineRef.current.getBoundingClientRect().top;
+
+    if (top <= 300) {
+      handleLineHeight(20);
+    }
+    if (top <= 0) {
+      handleLineHeight(40);
+    }
+    if (top <= -300) {
+      handleLineHeight(60);
+    }
+    if (top <= -600) {
+      handleLineHeight(80);
+    }
+    if (top <= -1000) {
+      handleLineHeight(100);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <Wrapper onScroll={handleScroll}>
+      <Line height={calcHeight} ref={lineRef} />
+    </Wrapper>
+  );
+};
 
 const Line = styled.div`
   background-color: var(--brand-button-primary-accent);
-  height: 100%;
+  height: ${(p) => p.height}%;
+  transition: height 320ms;
   width: 2px;
 `;
 
