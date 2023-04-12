7a064c19a821ee6343eade84d711200e2ba2d954
Tabatha Zeitke <tzeitke@newrelic.com>
2023-04-12 15:58:47 -0700

    feat: create img WebP conversion script
    - this change adds `yarn convert-to-webp` command
    - this checks for PNG images, converts them to WebP,
    then updates markdown references

--- scripts/convertPNGs.mjs
+++ scripts/convertPNGs.mjs
@@ -1,15 +1,82 @@
 #!/usr/bin/env node
-import glob from 'glob';
+/* eslint-disable no-console */
+
+import { rm, readFile, writeFile } from 'fs/promises';
+import { glob } from 'glob';
+import meow from 'meow';
 import webp from 'webp-converter';
+import { promisify } from 'util';
+import { exec as callback_exec } from 'child_process';
 
-const pngs = await glob('**/*.png');
+const cli = meow(
+  `
+	Usage
+	  $ yarn convert-to-webp <flag>
 
-const swapExtension = (fullPath) => {
-  const pathParts = fullPath.split('.');
+	Options
+	  --global, -g  run on all PNGs
 
-  return pathParts.slice(0, -1).concat('webp').join('.');
+`,
+  {
+    importMeta: import.meta,
+    flags: {
+      global: {
+        type: 'boolean',
+        alias: 'g',
+      },
+    },
+  }
+);
+
+const exec = promisify(callback_exec);
+
+const stagedFiles = (
+  await exec('git diff --diff-filter=d --cached --name-only')
+).stdout
+  .split(/[\r\n|\n|\r]/)
+  .filter(String);
+
+const stagedPNGs = stagedFiles.filter((file) =>
+  file.toLocaleLowerCase().endsWith('.png')
+);
+const stagedMDs = stagedFiles.filter((file) =>
+  /.mdx?$/.test(file.toLocaleLowerCase())
+);
+
+const allPNGs = await glob('**/*.png');
+const allMDs = await glob('**/*.{md,mdx}');
+
+const pngsToConvert = cli.flags.global ? allPNGs : stagedPNGs;
+const mdToConvert = cli.flags.global ? allMDs : stagedMDs;
+
+const updateMarkdownReferences = async (mdArray) => {
+  for (const file of mdArray) {
+    const imgImportRegEx = /(?:(\(.+)\.png|(import.*from .+)\.png)/g;
+    const contents = await readFile(file, { encoding: 'utf8' });
+    const newContents = contents.replaceAll(imgImportRegEx, '$1$2.webp');
+    await writeFile(file, newContents).then(() =>
+      console.log(`✨  Updated image reference(s) in \x1b[33m${file}\x1b[0m`)
+    );
+  }
 };
 
-for (const pngPath of pngs) {
-  webp.cwebp(pngPath, swapExtension(pngPath));
-}
+const convertPNGs = async (pngArray) => {
+  const swapExtension = (fullPath) => {
+    const pathParts = fullPath.split('.');
+
+    return pathParts.slice(0, -1).concat('webp').join('.');
+  };
+
+  for (const pngPath of pngArray) {
+    const webpPath = swapExtension(pngPath);
+    await webp
+      .cwebp(pngPath, webpPath)
+      .then(() => rm(pngPath))
+      .then(() =>
+        console.log(`✨  Converted \x1b[33m${pngPath}\x1b[0m to ✨WebP✨`)
+      );
+  }
+};
+
+await convertPNGs(pngsToConvert);
+updateMarkdownReferences(mdToConvert);
