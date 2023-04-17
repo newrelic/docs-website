2a8682c0b0072eafa9f9d2ca20afd94d7993e61a
Sunny Zanchi <sunnyz@hey.com>
2023-04-17 17:57:44 -0400

    include jpgs in webp conversion script (#12652)
    * check literal `.` in `stagedMDs` regex
    
    * use `continue` instead of `break` to avoid early exit
    
    * update convert-to-webp script to include jpgs
    
    * add error logging to convert-to-webp
    
    * run Prettier

--- scripts/convertPNGs.mjs
+++ scripts/convertPNGs.mjs
@@ -15,10 +15,11 @@ const cli = meow(
 	  $ yarn convert-to-webp <flag>
 
 	Options
-	  --global, -g  run on all PNGs
+	  --global, -g  run on all images in the repo, not just staged images
 
 `,
   {
+    description: 'Convert staged PNG and JPG images to WebP.',
     importMeta: import.meta,
     flags: {
       global: {
@@ -37,50 +38,76 @@ const stagedFiles = (
   .split(/[\r\n|\n|\r]/)
   .filter(String);
 
-const stagedPNGs = stagedFiles.filter((file) =>
-  file.toLocaleLowerCase().endsWith('.png')
+const imgExtensions = ['jpg', 'jpeg', 'png'];
+const imgRegex = new RegExp(`\.(?:${imgExtensions.join('|')})$`);
+
+const stagedImages = stagedFiles.filter((file) =>
+  imgRegex.test(file.toLocaleLowerCase())
 );
 const stagedMDs = stagedFiles.filter((file) =>
-  /.mdx?$/.test(file.toLocaleLowerCase())
+  /\.mdx?$/.test(file.toLocaleLowerCase())
 );
 
-const allPNGs = await glob('**/*.png');
-const allMDsAndJSs = await glob('**/*.{md,mdx,js}');
+const allImages = await glob(`**/*.{${imgExtensions.join(',')}}`);
+const allMDsAndJSs = await glob('src/**/*.{md,mdx,js}');
 
-const pngsToConvert = cli.flags.global ? allPNGs : stagedPNGs;
+const imagesToConvert = cli.flags.global ? allImages : stagedImages;
 const mdToConvert = cli.flags.global ? allMDsAndJSs : stagedMDs;
 
 const updateMarkdownReferences = async (mdArray) => {
+  const imgImportRegEx = new RegExp(
+    // prettier-ignore
+    `(?:(\\(.+)\\.(?:${imgExtensions.join('|')})|(import.*from .+)\\.(?:${imgExtensions.join('|')}))`,
+    'g'
+  );
+
+  console.log(
+    `⏳  Checking references in ${mdArray.length} markdown & JS files`
+  );
   for (const file of mdArray) {
-    const imgImportRegEx = /(?:(\(.+)\.png|(import.*from .+)\.png)/g;
     const contents = await readFile(file, { encoding: 'utf8' });
     if (!imgImportRegEx.test(contents)) {
-      break;
+      continue;
     }
     const newContents = contents.replaceAll(imgImportRegEx, '$1$2.webp');
-    await writeFile(file, newContents).then(() =>
-      console.log(`✨  Updated image reference(s) in \x1b[33m${file}\x1b[0m`)
-    );
-  }
-};
-
-const convertPNGs = async (pngArray) => {
-  const swapExtension = (fullPath) => {
-    const pathParts = fullPath.split('.');
-
-    return pathParts.slice(0, -1).concat('webp').join('.');
-  };
-
-  for (const pngPath of pngArray) {
-    const webpPath = swapExtension(pngPath);
-    await webp
-      .cwebp(pngPath, webpPath)
-      .then(() => rm(pngPath))
+    await writeFile(file, newContents)
       .then(() =>
-        console.log(`✨  Converted \x1b[33m${pngPath}\x1b[0m to ✨WebP✨`)
-      );
+        console.log(`✨  Updated image reference(s) in \x1b[33m${file}\x1b[0m`)
+      )
+      .catch((err) => {
+        console.log(
+          `😵  Failed trying to update reference(s) in \x1b[33m${file}\x1b[0m:`
+        );
+        console.log(err);
+        process.exitCode = 1;
+      });
   }
 };
 
-await convertPNGs(pngsToConvert);
+const convertImages = async (imageArray) => {
+  console.log(`⏳  Converting ${imageArray.length} images`);
+  for (const imagePath of imageArray) {
+    const webpPath = swapExtension(imagePath);
+    await webp
+      .cwebp(imagePath, webpPath)
+      .then(() => rm(imagePath))
+      .then(() =>
+        console.log(`✨  Converted \x1b[33m${imagePath}\x1b[0m to ✨WebP✨`)
+      )
+      .catch((err) => {
+        console.log(
+          `😵  Failed trying to convert \x1b[33m${imagePath}\x1b[0m:`
+        );
+        console.log(err);
+        process.exitCode = 1;
+      });
+  }
+};
+
+const swapExtension = (fullPath) => {
+  const pathParts = fullPath.split('.');
+  return pathParts.slice(0, -1).concat('webp').join('.');
+};
+
+await convertImages(imagesToConvert);
 updateMarkdownReferences(mdToConvert);
