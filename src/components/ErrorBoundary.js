9a39fc1133e9c1b9fc30b0256341c2462edc03ac
Liz Baker <lbaker@newrelic.com>
2023-04-24 16:08:11 -0700

    chore: Create error boundary and message

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,28 @@
+/* eslint-disable react/sort-comp */
+import React from 'react';
+import ErrorMessage from './ErrorMessage';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch() {
+    this.props.callTessen();
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorMessage />;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
