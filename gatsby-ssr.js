f18046db453e90f4da44b7b6e9f28afc6f0ba8b0
Tabatha Zeitke <tzeitke@newrelic.com>
2023-05-16 14:22:27 -0700

    chore: remove marketing scripts for performance testing

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,5 +4,4 @@
  * See: https://www.gatsbyjs.org/   docs/ssr-apis/
  */
 
-export { default as onRenderBody } from './gatsby/on-render-body';
 export { default as onPreRenderHTML } from './gatsby/on-pre-render-html';
