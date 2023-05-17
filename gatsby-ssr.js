bf01dd18a38d630c2870da0bee309738e2fd7de2
Tabatha Zeitke <tzeitke@newrelic.com>
2023-05-17 15:17:29 -0700

    chore: add back VWO script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,5 @@
  * See: https://www.gatsbyjs.org/   docs/ssr-apis/
  */
 
+export { default as onRenderBody } from './gatsby/on-render-body';
 export { default as onPreRenderHTML } from './gatsby/on-pre-render-html';
--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-useless-escape */
+// for VWO script linting errors
+
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -9,6 +12,31 @@ const METADATA = [
     content: 'He_vizRXYX_mUhwBe3BmyaMxNnVRAZbq_Jtm2A0e4WY',
   },
 ];
+
+const visualWebsiteOptimizer = (location) => {
+  const { pathname } = location;
+  const homepage = '/';
+  const signup =
+    '/docs/accounts/accounts-billing/account-setup/create-your-new-relic-account/';
+
+  if (pathname === homepage || pathname === signup) {
+    return (
+      <script type="text/javascript" id="vwoCode">
+        {`window._vwo_code=window._vwo_code || (function() {
+        var account_id=680279,
+        version = 1.5,
+        settings_tolerance=2000,
+        library_tolerance=2500,
+        use_existing_jquery=false,
+        is_spa=1,
+        hide_element='body',
+        hide_element_style = 'opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important',
+        /* DO NOT EDIT BELOW THIS LINE */
+        f=false,d=document,vwoCodeEl=d.querySelector('#vwoCode'),code={use_existing_jquery:function(){return use_existing_jquery},library_tolerance:function(){return library_tolerance},hide_element_style:function(){return'{'+hide_element_style+'}'},finish:function(){if(!f){f=true;var e=d.getElementById('_vis_opt_path_hides');if(e)e.parentNode.removeChild(e)}},finished:function(){return f},load:function(e){var t=d.createElement('script');t.fetchPriority='high';t.src=e;t.type='text/javascript';t.innerText;t.onerror=function(){_vwo_code.finish()};d.getElementsByTagName('head')[0].appendChild(t)},getVersion:function(){return version},getMatchedCookies:function(e){var t=[];if(document.cookie){t=document.cookie.match(e)||[]}return t},getCombinationCookie:function(){var e=code.getMatchedCookies(/(?:^|;)\s?(_vis_opt_exp_\d+_combi=[^;$]*)/gi);e=e.map(function(e){try{var t=decodeURIComponent(e);if(!/_vis_opt_exp_\d+_combi=(?:\d+,?)+\s*$/.test(t)){return''}return t}catch(e){return''}});var i=[];e.forEach(function(e){var t=e.match(/([\d,]+)/g);t&&i.push(t.join('-'))});return i.join('|')},init:function(){if(d.URL.indexOf('__vwo_disable__')>-1)return;window.settings_timer=setTimeout(function(){_vwo_code.finish()},settings_tolerance);var e=d.createElement('style'),t=hide_element?hide_element+'{'+hide_element_style+'}':'',i=d.getElementsByTagName('head')[0];e.setAttribute('id','_vis_opt_path_hides');vwoCodeEl&&e.setAttribute('nonce',vwoCodeEl.nonce);e.setAttribute('type','text/css');if(e.styleSheet)e.styleSheet.cssText=t;else e.appendChild(d.createTextNode(t));i.appendChild(e);var n=this.getCombinationCookie();this.load('https://dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&f='+ +is_spa+'&vn='+version+(n?'&c='+n:''));return settings_timer}};window._vwo_settings_timer = code.init();return code;}());`}
+      </script>
+    );
+  }
+};
 
 const isStyleGuidePage = (url) => {
   return url.includes('docs/style-guide');
@@ -84,6 +112,8 @@ const DocsSiteSeo = ({
     {(description || title) && (
       <meta name="description" content={description || title} />
     )}
+
+    {visualWebsiteOptimizer(location)}
   </SEO>
 );
 
