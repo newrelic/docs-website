484c68d8a8273b8b1cb669766b1047aa7f522981
Tabatha Zeitke <tzeitke@newrelic.com>
2023-06-12 12:09:44 -0700

    chore: resolve linting errors

--- src/components/ContributorNote.js
+++ src/components/ContributorNote.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import PropTypes from 'prop-types';
-
 /* In place of comments, this component is intended to be used by authors
  * to leave important notes about MDX content for future contributors.
  *
