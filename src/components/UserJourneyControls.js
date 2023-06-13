2443774151f580711946e2ab4b65584748c823e5
Liz Baker <lbaker@newrelic.com>
2023-06-13 15:23:07 -0700

    fix: Make user journey steps optional

--- src/components/UserJourneyControls.js
+++ src/components/UserJourneyControls.js
@@ -24,78 +24,82 @@ const UserJourneyControls = ({ previousStep, nextStep, className }) => {
 
   return (
     <JourneyContainer>
-      <JourneyLink
-        base={Surface.BASE.SECONDARY}
-        to={previousStep.path}
-        interactive
-        instrumentation={{
-          category: 'UserJourney',
-          eventName: 'previousStepClick',
-          path: previousStep.path,
-        }}
-        className={className}
-        onMouseEnter={triggerLeft}
-      >
-        <InnerContainer>
-          <animated.div style={leftButton}>
-            <Icon
-              name="fe-arrow-left"
-              size="3rem"
-              css={css`
-                color: var(--system-text-primary);
-                margin-right: 2rem;
-              `}
-            />
-          </animated.div>
-          <TextContainer>
-            <h3>{previousStep.title}</h3>
-            <p>{previousStep.body}</p>
-          </TextContainer>
-        </InnerContainer>
-      </JourneyLink>
-      <JourneyLink
-        base={Surface.BASE.SECONDARY}
-        to={nextStep.path}
-        interactive
-        instrumentation={{
-          category: 'UserJourney',
-          eventName: 'nextStepClick',
-          path: nextStep.path,
-        }}
-        className={className}
-        css={css`
-          border: solid 2px var(--brand-button-primary-accent);
-          &:hover {
-            color: var(--primary-text-color);
-            box-shadow: -2px -2px var(--brand-button-primary-accent),
-              2px -2px var(--brand-button-primary-accent),
-              -2px 2px var(--brand-button-primary-accent),
-              2px 2px var(--brand-button-primary-accent), var(--shadow-3);
-          }
-        `}
-        onMouseEnter={triggerRight}
-      >
-        <InnerContainer
-          css={css`
-            justify-content: space-between;
-          `}
+      {previousStep && (
+        <JourneyLink
+          base={Surface.BASE.SECONDARY}
+          to={previousStep.path}
+          interactive
+          instrumentation={{
+            category: 'UserJourney',
+            eventName: 'previousStepClick',
+            path: previousStep.path,
+          }}
+          className={className}
+          onMouseEnter={triggerLeft}
         >
-          <TextContainer>
-            <h3>{nextStep.title}</h3>
-            <p>{nextStep.body}</p>
-          </TextContainer>
-          <animated.div style={rightButton}>
-            <Icon
-              name="fe-arrow-right"
-              size="3rem"
-              css={css`
-                color: var(--primary-text-color);
-                margin-left: 2rem;
-              `}
-            />
-          </animated.div>
-        </InnerContainer>
-      </JourneyLink>
+          <InnerContainer>
+            <animated.div style={leftButton}>
+              <Icon
+                name="fe-arrow-left"
+                size="3rem"
+                css={css`
+                  color: var(--system-text-primary);
+                  margin-right: 2rem;
+                `}
+              />
+            </animated.div>
+            <TextContainer>
+              <h3>{previousStep.title}</h3>
+              <p>{previousStep.body}</p>
+            </TextContainer>
+          </InnerContainer>
+        </JourneyLink>
+      )}
+      {nextStep && (
+        <JourneyLink
+          base={Surface.BASE.SECONDARY}
+          to={nextStep.path}
+          interactive
+          instrumentation={{
+            category: 'UserJourney',
+            eventName: 'nextStepClick',
+            path: nextStep.path,
+          }}
+          className={className}
+          css={css`
+            border: solid 2px var(--brand-button-primary-accent);
+            &:hover {
+              color: var(--primary-text-color);
+              box-shadow: -2px -2px var(--brand-button-primary-accent),
+                2px -2px var(--brand-button-primary-accent),
+                -2px 2px var(--brand-button-primary-accent),
+                2px 2px var(--brand-button-primary-accent), var(--shadow-3);
+            }
+          `}
+          onMouseEnter={triggerRight}
+        >
+          <InnerContainer
+            css={css`
+              justify-content: space-between;
+            `}
+          >
+            <TextContainer>
+              <h3>{nextStep.title}</h3>
+              <p>{nextStep.body}</p>
+            </TextContainer>
+            <animated.div style={rightButton}>
+              <Icon
+                name="fe-arrow-right"
+                size="3rem"
+                css={css`
+                  color: var(--primary-text-color);
+                  margin-left: 2rem;
+                `}
+              />
+            </animated.div>
+          </InnerContainer>
+        </JourneyLink>
+      )}
     </JourneyContainer>
   );
 };
