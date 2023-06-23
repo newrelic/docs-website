dcc87d9b7840e9be45dc1ddbfcc47e1c0a3c1c6e
Clark McAdoo <jmcadoo@newrelic.com>
2023-06-23 09:49:21 -0500

    fix: clear attribute dictionary json, update script for testing

--- scripts/actions/updateAttributeDictionaryJson.mjs
+++ scripts/actions/updateAttributeDictionaryJson.mjs
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 import fetch from 'node-fetch';
-import * as fs from 'fs/promises';
+import fs from 'fs';
 
 const NERDGRAPH_API_URL = 'https://staging-api.newrelic.com/graphql';
 const JSON_FILE_PATH = './attribute-dictionary.json';
@@ -24,13 +24,34 @@ const GQL_QUERY = `
   }
 `;
 
-const updatedJson = await fetch(NERDGRAPH_API_URL, {
-  method: 'POST',
-  headers: {
-    'Api-Key': process.env.API_KEY,
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ query: GQL_QUERY }),
-}).then((res) => res.json());
+async function updateJson() {
+  try {
+    const updatedJson = await fetch(NERDGRAPH_API_URL, {
+      method: 'POST',
+      headers: {
+        'Api-Key': process.env.API_KEY,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query: GQL_QUERY }),
+    }).then((res) => res.json());
 
-fs.writeFile(JSON_FILE_PATH, JSON.stringify(updatedJson, null, 2));
+    const formattedJson = JSON.stringify(updatedJson, null, 2);
+    console.log('Fetch successful!');
+
+    const compareJson = fs.readFileSync(JSON_FILE_PATH, { encoding: 'utf-8' });
+
+    const hasUpdates =
+      compareJson === formattedJson
+        ? 'No updates to attribute dictionary'
+        : 'Adding updates for attribute dictionary json';
+
+    console.log(hasUpdates);
+
+    fs.writeFileSync(JSON_FILE_PATH, formattedJson);
+  } catch (error) {
+    console.error('Issue with fetching attribute dictionary:', error);
+    process.exit(1);
+  }
+}
+
+updateJson();
