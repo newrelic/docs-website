e7528a32df277b33f9079d71ad484cc2b6150bb9
Clark McAdoo <jmcadoo@newrelic.com>
2023-06-29 17:07:01 -0500

    feat: create EOL page component

--- src/components/EolPage.js
+++ src/components/EolPage.js
@@ -1,14 +1,68 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { format, parseISO } from 'date-fns';
+import { ja, ko } from 'date-fns/locale';
 
 const AGENT_RELEASE_NOTES_URL =
   'https://docs-assets.newrelic-external.com/release-notes-json';
 
-const EolPage = () => {
-  useEffect(() => {
-    const data = fetch(AGENT_RELEASE_NOTES_URL);
-    console.log('test');
+const EolPage = ({ agent, locale }) => {
+  const [releaseNotes, setReleaseNotes] = useState([]);
+
+  useEffect(async () => {
+    const data = await fetch(AGENT_RELEASE_NOTES_URL, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const dataJson = await data.json();
+    setReleaseNotes(dataJson);
   }, []);
-  return <h1>Hello World World!</h1>;
+
+  const sortDateDesc = (a, b) => {
+    const aDate = parseISO(a.date);
+    const bDate = parseISO(b.date);
+
+    if (aDate < bDate) {
+      return 1;
+    }
+    if (aDate > bDate) {
+      return -1;
+    }
+  };
+
+  const formatDate = (date, locale) => {
+    const iso = parseISO(date);
+    if (locale === 'ja') {
+      return format(iso, 'PPP', { locale: ja });
+    }
+    if (locale === 'ko') {
+      return format(iso, 'PPP', { locale: ko });
+    }
+    return format(iso, 'PP');
+  };
+
+  if (releaseNotes.length > 0) {
+    const table = releaseNotes
+      .filter((note) => note.agent === agent)
+      .sort(sortDateDesc);
+
+    return (
+      <tbody>
+        {table.map((note) => {
+          return (
+            <tr>
+              <td>v{note.version}</td>
+              <td>{formatDate(note.date, locale)}</td>
+              <td>{formatDate(note.eolDate, locale)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    );
+  }
+  return <p>Loading...</p>;
 };
 
 export default EolPage;
