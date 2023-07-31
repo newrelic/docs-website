b9ddbcfbb5c53baf76706773b4cc57b1564fd00a
a-sassman <asassman@newrelic.com>
2023-07-31 07:44:44 -0700

    fix(docs): sdk doc edits

--- scripts/utils/migrate/instruction-set.js
+++ scripts/utils/migrate/instruction-set.js
@@ -511,7 +511,7 @@ module.exports = [
       'Android SDK API',
     ],
     node: {
-      path: '/docs/mobile-monitoring/new-relic-mobile-android/android-sdk-api',
+      path: '/docs/mobile-monitoring/new-relic-mobile/mobile-sdk/mobile-sdk-api-guide/',
     },
   },
   {
@@ -523,7 +523,7 @@ module.exports = [
       'iOS SDK API',
     ],
     node: {
-      path: '/docs/mobile-monitoring/new-relic-mobile-ios/ios-sdk-api',
+      path: '/docs/mobile-monitoring/new-relic-mobile/mobile-sdk/mobile-sdk-api-guide/',
     },
   },
   {
