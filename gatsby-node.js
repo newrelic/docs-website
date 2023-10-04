3f0b6ec1e80c1ea045af2129d527b41b99cca811
Clinton <clinton.langosch@gmail.com>
2023-10-04 10:39:27 -0700

    chore: disble DSGto get successful build

--- gatsby-node.js
+++ gatsby-node.js
@@ -289,7 +289,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
           createPage,
           disableSwiftype: !i18nNode,
         },
-        true // enable DSG
+        false // disable DSG
       );
     });
   });
