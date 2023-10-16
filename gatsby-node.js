e39a41bf9542b5647097856a0ff40b67a35f5475
Liz Baker <lbaker@newrelic.com>
2023-10-16 11:10:29 -0700

    turn dsg back on to try a merge

--- gatsby-node.js
+++ gatsby-node.js
@@ -146,7 +146,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
           createPage,
           disableSwiftype: !i18nNode,
         },
-        false // enable DSG
+        true // enable DSG
       );
     });
   });
