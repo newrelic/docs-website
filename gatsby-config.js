4cf8d2ef4723f54cb822cf5c0d97f1b673e77d02
Clinton <clinton.langosch@gmail.com>
2023-10-17 14:36:30 -0700

    chore: remove unnecessary meta redirect package

--- gatsby-config.js
+++ gatsby-config.js
@@ -322,7 +322,6 @@ module.exports = {
 
     'gatsby-source-nav',
     'gatsby-source-install-config',
-    // 'gatsby-plugin-meta-redirect',
     {
       resolve: 'gatsby-plugin-gatsby-cloud',
       options: {
