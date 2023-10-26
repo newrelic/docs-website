99d4c5a6c6eabdb64e2b2da7a5c10648a7af2b2a
Clark McAdoo <jmcadoo@newrelic.com>
2023-10-26 13:06:08 -0500

    feat: working script with progress bar

--- scripts/docsAndLastDateEdited.mjs
+++ scripts/docsAndLastDateEdited.mjs
@@ -1,18 +1,34 @@
 #! /usr/bin/env node
 
+import { execSync } from 'child_process';
 import { glob } from 'glob10';
-import { statSync, writeFile } from 'fs';
+import { writeFile } from 'fs';
 import { stringify } from 'csv-stringify';
+import cliProgress from 'cli-progress';
+
+console.log('Building CSV file...');
+console.time('csvCreation');
 
 const allDocs = await glob('src/content/docs/**/*.mdx', {
   ignore: '**/index.mdx',
 });
 
-const allDocsAndDates = allDocs.map((doc) => {
-  // mtime is the last modified time of a particular file
-  // https://nodejs.org/api/fs.html#statsmtime
-  const modifiedDate = statSync(doc).mtime;
-  return [doc, modifiedDate.toDateString()];
+const progressBar = new cliProgress.SingleBar(
+  {},
+  cliProgress.Presets.shades_classic
+);
+progressBar.start(allDocs.length, 0);
+
+const allDocsAndDates = allDocs.map((doc, i) => {
+  const gitLogModifiedDate = execSync(
+    `git log -1 --pretty=format:%aI ${doc}`
+  ).toString();
+
+  const dateModified = new Date(gitLogModifiedDate);
+
+  progressBar.update(i);
+
+  return [doc, dateModified.toDateString()];
 });
 
 stringify(allDocsAndDates, function (err, output) {
@@ -23,6 +39,8 @@ stringify(allDocsAndDates, function (err, output) {
       );
     } else {
       console.log('CSV Created!');
+      progressBar.stop();
+      console.timeEnd('csvCreation');
     }
   });
 });
