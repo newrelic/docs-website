08d8973c08d94bf05abcfd1077907f8449f17bbf
Liz Baker <lbaker@newrelic.com>
2023-11-14 15:59:58 -0800

    chore: Remove onClientEntry

--- gatsby/on-client-entry.js
+++
@@ -1,9 +0,0 @@
-const onClientEntry = (_, themeOptions) => {
-  if (themeOptions.survey) {
-    window._nr_survey = {
-      environment: `${themeOptions.survey.environment}`,
-      reCaptchaToken: `${themeOptions.survey.reCaptchaToken}`,
-    };
-  }
-};
-export default onClientEntry;
