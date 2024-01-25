4fa6cd3c893de0911434d88758037f45a8203611
Clark McAdoo <jmcadoo@newrelic.com>
2024-01-25 16:45:24 -0600

    fix: add segment config

--- gatsby-config.js
+++ gatsby-config.js
@@ -475,6 +475,11 @@ module.exports = {
             '6LeGFt8UAAAAANfnpE8si2Z6NnAqYKnPAYgMpStu',
         },
         newRelicRequestingServicesHeader: 'docs-website',
+        segment: {
+          segmentWriteKey: 'noviNOFjASOSPcSEAkwoRxOt0Y1719KD',
+          section: 'docs',
+          platform: 'docs_pages',
+        },
       },
     },
   ],
