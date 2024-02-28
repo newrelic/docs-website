38775aa2ac7656cf56103510086c3bf7214f8fc5
Liz Baker <lbaker@newrelic.com>
2024-02-28 09:26:35 -0800

    chore: Add allFiles option to script

--- scripts/actions/manual-add-files-to-translation-queue.js
+++ scripts/actions/manual-add-files-to-translation-queue.js
@@ -2,6 +2,7 @@ const { Command } = require('commander');
 const {
   addFilesToTranslationQueue,
 } = require('./add-files-to-translation-queue');
+const { getFilesRecursively } = require('../utils/getFiles');
 
 const getCommandLineOptions = () => {
   const program = new Command();
@@ -14,6 +15,10 @@ const getCommandLineOptions = () => {
     .option(
       '-l, --locale <locale>',
       'Specifiy specific language to be sent to smartling for translation'
+    )
+    .option(
+      '-all, --allFiles',
+      'Boolean to send all files for translation (for new languages)'
     );
   program.parse(process.argv);
   return program.opts();
@@ -25,11 +30,19 @@ const parseFilesString = (filesString) =>
 const main = async () => {
   const options = getCommandLineOptions();
   const filesString = options.files || null;
-
+  const allFiles = options.allFiles || null;
+  console.log(options);
   if (filesString) {
+    console.log('shouldnt be here');
     const fileNames = parseFilesString(filesString);
+    // await addFilesToTranslationQueue(fileNames, options);
+  } else if (allFiles) {
+    console.log('should be here');
+    const fileNames = getFilesRecursively('src/content/docs');
+    console.log(fileNames.length, fileNames[0]);
     await addFilesToTranslationQueue(fileNames, options);
   }
+
   process.exit(0);
 };
 
