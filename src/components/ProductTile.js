6bddc6130b796d09951ad6202bae1f2df76eb533
Clark McAdoo <jmcadoo@newrelic.com>
2024-04-15 11:31:26 -0500

    fix: get animation looking better

--- src/components/ProductTile.js
+++ src/components/ProductTile.js
@@ -18,7 +18,6 @@ const ProductTile = ({ children, to, title, icon }) => {
           display: flex;
           flex-direction: column;
           height: 100%;
-          justify-content: space-between;
           left: 0;
           padding: 1.375rem;
           position: absolute;
@@ -29,19 +28,25 @@ const ProductTile = ({ children, to, title, icon }) => {
 
           p {
             margin: 0;
+            overflow: visible;
+            width: 250px;
           }
 
           &:hover {
             color: var(--primary-text-color);
             height: 130%;
-            transform: translate(-15%, -15%);
+            transform: translate(-10%, -10%);
             transition: height 500ms, transform 500ms, width 500ms;
             width: 130%;
             z-index: 10;
 
+            .hover-hide {
+              display: none;
+            }
+
             .text {
               opacity: 1;
-              transition: opacity 200ms;
+              transition: opacity 500ms;
             }
           }
         `}
@@ -58,20 +63,24 @@ const ProductTile = ({ children, to, title, icon }) => {
           className='text'
           css={css`
             font-size: 0.875rem;
-            line-height: 1.15;
+            line-height: 1.5rem;
             opacity: 0;
-            transition: opacity 200ms;
+            transition: opacity 325ms;
           `}
         >
           {children}
         </p>
         <Icon
           name={icon}
-          size="2rem"
+          size="1rem"
+          className="hover-hide"
           css={css`
+            align-self: flex-end;
+            bottom: 1rem;
             flex: 1;
             max-height: 1.5625rem;
-            align-self: flex-end;
+            position: absolute;
+            right: 1rem;
           `}
         />
       </SurfaceLink>
