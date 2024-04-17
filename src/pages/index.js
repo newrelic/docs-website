7daf3bfd2f140ed8b6be63421d5398553eeeca41
Tabatha Zeitke <tzeitke@newrelic.com>
2024-04-17 14:50:19 -0500

    make product tile layout dynamic

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import {
   Link,
   Icon,
+  useLocale,
   useInstrumentedHandler,
   useTranslation,
 } from '@newrelic/gatsby-theme-newrelic';
@@ -17,6 +18,13 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import FeedbackModal from '../components/FeedbackModal';
 import { OnboardingContainer, OnboardingStep } from '../components/Onboarding';
 
+// to map over the items to generate the product tiles
+import enJson from '../i18n/translations/en/translation.json';
+import krJson from '../i18n/translations/kr/translation.json';
+import jpJson from '../i18n/translations/jp/translation.json';
+import esJson from '../i18n/translations/es/translation.json';
+import ptJson from '../i18n/translations/pt/translation.json';
+
 const HomePage = ({ data }) => {
   const {
     site: { layout },
@@ -24,6 +32,16 @@ const HomePage = ({ data }) => {
   const [showFeedbackModal, setShowFeedbackModal] = useState(true);
 
   const { t } = useTranslation();
+
+  const { locale } = useLocale();
+  const tileJson = (() => {
+    if (locale === 'en') return enJson;
+    if (locale === 'kr') return krJson;
+    if (locale === 'jp') return jpJson;
+    if (locale === 'es') return esJson;
+    if (locale === 'pt') return ptJson;
+    return enJson;
+  })();
 
   const mobileBreakpoint = '450px';
   const isMobileScreen = useMedia(`(max-width: ${mobileBreakpoint})`);
@@ -108,241 +126,20 @@ const HomePage = ({ data }) => {
           </DocTiles>
         </div>
       </Section>
-      <Section layout={layout}>
-        <SectionTitle title={t('home.productRow1.title')} />
-        <SectionContent>
-          <ProductTile
-            to="/docs/ai-monitoring/intro-to-ai-monitoring/"
-            title={t('home.productRow1.t1.title')}
-            icon="nr-ai-monitoring"
-          >
-            {t('home.productRow1.t1.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/apm/new-relic-apm/getting-started/introduction-apm/"
-            title={t('home.productRow1.t2.title')}
-            icon="nr-apm"
-          >
-            {t('home.productRow1.t2.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/browser/browser-monitoring/getting-started/introduction-browser-monitoring/"
-            title={t('home.productRow1.t3.title')}
-            icon="nr-browser"
-          >
-            {t('home.productRow1.t3.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/infrastructure/infrastructure-monitoring/get-started/get-started-infrastructure-monitoring/"
-            title={t('home.productRow1.t4.title')}
-            icon="nr-infrastructure"
-          >
-            {t('home.productRow1.t4.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/kubernetes-pixie/kubernetes-integration/get-started/introduction-kubernetes-integration/"
-            title={t('home.productRow1.t5.title')}
-            icon="nr-k8s-cluster"
-          >
-            {t('home.productRow1.t5.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/logs/get-started/get-started-log-management/"
-            title={t('home.productRow1.t6.title')}
-            icon="nr-logs"
-          >
-            {t('home.productRow1.t6.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/mobile-monitoring/new-relic-mobile/get-started/introduction-mobile-monitoring/"
-            title={t('home.productRow1.t7.title')}
-            icon="nr-mobile"
-          >
-            {t('home.productRow1.t7.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/mlops/get-started/intro-mlops/"
-            title={t('home.productRow1.t8.title')}
-            icon="nr-learning-models"
-          >
-            {t('home.productRow1.t8.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/network-performance-monitoring/get-started/npm-introduction/"
-            title={t('home.productRow1.t9.title')}
-            icon="nr-network-monitoring"
-          >
-            {t('home.productRow1.t9.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/serverless-function-monitoring/overview/"
-            title={t('home.productRow1.t10.title')}
-            icon="nr-ml-endpoints"
-          >
-            {t('home.productRow1.t10.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/synthetics/synthetic-monitoring/getting-started/get-started-synthetic-monitoring/"
-            title={t('home.productRow1.t11.title')}
-            icon="nr-synthetics"
-          >
-            {t('home.productRow1.t11.body')}
-          </ProductTile>
-        </SectionContent>
+      <Section
+        layout={layout}
+        css={css`
+          h3:nth-of-type(2) {
+              padding-top: 0;
+            }
+          }
+        `}
+      >
+        <SectionTitle title={t('home.productTilesHeader')} />
+
+        <ProductTileSection sections={tileJson.home.productTiles} />
       </Section>
-      <Section layout={layout}>
-        <SectionTitle title={t('home.productRow2.title')} />
-        <SectionContent>
-          <ProductTile
-            to="/docs/alerts-applied-intelligence/overview/"
-            title={t('home.productRow2.t1.title')}
-            icon="nr-alerts-ai"
-          >
-            {t('home.productRow2.t1.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/change-tracking/change-tracking-introduction/"
-            title={t('home.productRow2.t2.title')}
-            icon="nr-upstream-deployment"
-          >
-            {t('home.productRow2.t2.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/query-your-data/explore-query-data/get-started/introduction-querying-new-relic-data/"
-            title={t('home.productRow2.t3.title')}
-            icon="nr-dashboard"
-          >
-            {t('home.productRow2.t3.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/distributed-tracing/concepts/introduction-distributed-tracing/"
-            title={t('home.productRow2.t4.title')}
-            icon="nr-service-map"
-          >
-            {t('home.productRow2.t4.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/nrql/get-started/introduction-nrql-new-relics-query-language/"
-            title={t('home.productRow2.t5.title')}
-            icon="nr-query"
-          >
-            {t('home.productRow2.t5.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/service-level-management/intro-slm/"
-            title={t('home.productRow2.t6.title')}
-            icon="nr-horizontal-chart"
-          >
-            {t('home.productRow2.t6.body')}
-          </ProductTile>
-        </SectionContent>
-      </Section>
-      <Section layout={layout}>
-        <SectionTitle title={t('home.productRow3.title')} />
-        <SectionContent>
-          <ProductTile
-            to="/docs/codestream/start-here/what-is-codestream/"
-            title={t('home.productRow3.t1.title')}
-            icon="nr-notes-edit"
-          >
-            {t('home.productRow3.t1.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/tutorial-error-tracking/respond-outages/"
-            title={t('home.productRow3.t2.title')}
-            icon="nr-inbox"
-          >
-            {t('home.productRow3.t2.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/iast/introduction/"
-            title={t('home.productRow3.t3.title')}
-            icon="nr-iast"
-          >
-            {t('home.productRow3.t3.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/more-integrations/open-source-telemetry-integrations/opentelemetry/opentelemetry-introduction/"
-            title={t('home.productRow3.t4.title')}
-            icon="nr-needs-instrumentation"
-          >
-            {t('home.productRow3.t4.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/vulnerability-management/overview/"
-            title={t('home.productRow3.t5.title')}
-            icon="nr-vulnerability"
-          >
-            {t('home.productRow3.t5.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/website-performance-monitoring/increase-user-engagement/"
-            title={t('home.productRow3.t6.title')}
-            icon="nr-dashboard"
-          >
-            {t('home.productRow3.t6.body')}
-          </ProductTile>
-        </SectionContent>
-      </Section>
-      <Section layout={layout}>
-        <SectionTitle title={t('home.productRow4.title')} />
-        <SectionContent>
-          <ProductTile
-            to="/docs/release-notes/"
-            title={t('home.productRow4.t1.title')}
-            icon="nr-notes-edit"
-          >
-            {t('home.productRow4.t1.body')}
-          </ProductTile>
-          <ProductTile
-            to="/whats-new/"
-            title={t('home.productRow4.t2.title')}
-            icon="nr-notes-add"
-          >
-            {t('home.productRow4.t2.body')}
-          </ProductTile>
-        </SectionContent>
-      </Section>
-      <Section layout={layout}>
-        <SectionTitle title={t('home.productRow5.title')} />
-        <SectionContent>
-          <ProductTile
-            to="/docs/accounts/accounts-billing/account-setup/create-your-new-relic-account/"
-            title={t('home.productRow5.t1.title')}
-            icon="nr-user"
-          >
-            {t('home.productRow5.t1.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/data-apis/get-started/nrdb-horsepower-under-hood/"
-            title={t('home.productRow5.t2.title')}
-            icon="nr-area-chart"
-          >
-            {t('home.productRow5.t2.body')}
-          </ProductTile>
-          <ProductTile
-            to="/attribute-dictionary/"
-            title={t('home.productRow5.t3.title')}
-            icon="nr-bookmark"
-          >
-            {t('home.productRow5.t3.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/security/overview/"
-            title={t('home.productRow5.t4.title')}
-            icon="nr-private"
-          >
-            {t('home.productRow5.t4.body')}
-          </ProductTile>
-          <ProductTile
-            to="/docs/licenses/overview/"
-            title={t('home.productRow5.t5.title')}
-            icon="nr-attachment"
-          >
-            {t('home.productRow5.t5.body')}
-          </ProductTile>
-        </SectionContent>
-      </Section>
+
       {showFeedbackModal && !isMobileScreen && (
         <FeedbackModal onClose={() => setShowFeedbackModal(false)} />
       )}
@@ -425,6 +222,46 @@ const SectionContent = styled.div`
     grid-template-columns: 1fr;
   }
 `;
+
+const ProductTileSection = ({ sections }) => {
+  const tileSections = [];
+
+  for (const contents of Object.values(sections)) {
+    tileSections.push(
+      <>
+        <h3
+          css={css`
+            font-size: 1.25rem;
+            color: #7b838a;
+            font-weight: 400;
+            padding: 2.25rem 0 1.9375rem;
+            .dark-mode && {
+              color: #b5b6b6;
+            }
+          `}
+        >
+          {contents.sectionTitle.toUpperCase()}
+        </h3>
+        <SectionContent>
+          {contents.tiles.map((tile, i) => {
+            return (
+              <ProductTile
+                key={i}
+                to={tile.to}
+                title={tile.title}
+                icon={tile.icon}
+              >
+                {tile.text}
+              </ProductTile>
+            );
+          })}
+        </SectionContent>
+      </>
+    );
+  }
+  return tileSections;
+};
+
 const SectionTitle = ({ title, icon, to }) => {
   const handleClick = useInstrumentedHandler({
     eventName: 'sectionTitleClick',
