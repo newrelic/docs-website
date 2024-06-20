6ddc85227ae67b93a11cacbc2a5fecc626c4453b
Liz Baker <lbaker@newrelic.com>
2024-06-20 09:47:09 -0700

    chore: Add link check script and config
    Co-authored-by: jack-berg <jberg@newrelic.com>

---
+++ scripts/actions/check-links/check-links.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+find ./src/content/docs/browser/new-relic-browser/lab \
+ -type f \
+ -name '*.mdx' \
+ -print0 | xargs -0 -n1 npx markdown-link-check --quiet --config $(dirname "$0")/markdown-link-check-config.json >> $(dirname "$0")/broken-links.txt
+
+ exec cat ./$(dirname "$0")/broken-links.txt | uniq >> $(dirname "$0")/broken-links-filter.txt
