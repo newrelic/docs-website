3032f156f126cb1db14291299956dab19b9e84ca
Tabatha Zeitke <tzeitke@newrelic.com>
2024-07-31 13:17:57 -0500

    feat: update ES pages

--- scripts/updateImgSrc.js
+++ scripts/updateImgSrc.js
@@ -129,7 +129,7 @@ const runConvertImages = async (paths = []) => {
   }
 
   if (paths.length === 0) {
-    filePaths = glob.sync('src/install/**/*.mdx');
+    filePaths = glob.sync('src/i18n/content/es/**/*.mdx');
   }
 
   const allResults = await Promise.all(
