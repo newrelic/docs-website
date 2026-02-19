201b69c11014d51a0141eb48c41cc10e351afca8
akashreddy <akashreddy@newrelic.com>
2026-02-19 18:55:06 +0530

    feat: Add SAP Monitoring Icon

--- src/@newrelic/gatsby-theme-newrelic/icons/newrelic/software.js
+++ src/@newrelic/gatsby-theme-newrelic/icons/newrelic/software.js
@@ -14,10 +14,10 @@ const SoftwareIcon = (props) => (
     `}
   >
     <path d="M8.25 9H3V4h9L8.25 9z" />
-    <path 
-      fill-rule="evenodd" 
-      d="M13 1c1.1 0 2 .9 2 2v7c0 1.1-.9 2-2 2H8v2h3v1H4v-1h3v-2H2c-1.1 0-2-.9-2-2V3c0-1.1.9-2 2-2h11zM2 2c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h11c.6 0 1-.4 1-1V3c0-.6-.4-1-1-1H2z" 
-      clip-rule="evenodd"
+    <path
+      fillRule="evenodd"
+      d="M13 1c1.1 0 2 .9 2 2v7c0 1.1-.9 2-2 2H8v2h3v1H4v-1h3v-2H2c-1.1 0-2-.9-2-2V3c0-1.1.9-2 2-2h11zM2 2c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h11c.6 0 1-.4 1-1V3c0-.6-.4-1-1-1H2z"
+      clipRule="evenodd"
     />
   </SVG>
 );
